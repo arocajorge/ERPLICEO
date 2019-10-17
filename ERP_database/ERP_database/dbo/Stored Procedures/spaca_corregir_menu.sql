230b38ec327eb935f8e4b36d11993b101f1444e2
WendyCherres <wendycherres@gmail.com>
2019-10-17 17:12:59 -0500

    ..

---
+++ ERP_database/ERP_database/dbo/Stored Procedures/spaca_corregir_menu.sql
@@ -0,0 +1,68 @@
+﻿
+CREATE PROCEDURE [dbo].[spaca_corregir_menu]
+(
+@IdEmpresa int,
+@IdSede int, 
+@IdUsuario varchar(20)
+)
+AS
+insert into aca_Menu_x_aca_Sede 
+(IdEmpresa,IdSede,IdMenu,Observacion)
+select @IdEmpresa, @IdSede, menu.IdMenuPadre, null
+from seg_Menu as menu
+join aca_Menu_x_aca_Sede as me
+on me.IdMenu = menu.IdMenu
+where me.IdEmpresa = @IdEmpresa 
+and menu.IdMenuPadre != 0
+and not exists(
+select * from aca_Menu_x_aca_Sede as f
+where f.IdEmpresa = @IdEmpresa 
+and f.IdMenu = menu.IdMenuPadre
+)
+group by menu.IdMenuPadre
+
+insert into aca_Menu_x_aca_Sede
+select @IdEmpresa,@IdSede, menu.IdMenuPadre, null
+from seg_Menu as menu
+join aca_Menu_x_aca_Sede as me
+on me.IdMenu = menu.IdMenu
+where me.IdEmpresa = @IdEmpresa 
+and menu.IdMenuPadre != 0
+and not exists(
+select * from aca_Menu_x_aca_Sede as f
+where f.IdEmpresa = @IdEmpresa 
+and f.IdMenu = menu.IdMenuPadre
+)
+group by menu.IdMenuPadre
+
+insert into aca_Menu_x_seg_usuario (IdEmpresa,IdSede,IdMenu,IdUsuario,Perfil)
+select @IdEmpresa, @IdSede, menu.IdMenuPadre, @IdUsuario, 0
+from seg_Menu as menu
+join aca_Menu_x_seg_usuario as me
+on me.IdMenu = menu.IdMenu
+where me.IdEmpresa = @IdEmpresa 
+and me.IdUsuario = @IdUsuario
+and menu.IdMenuPadre != 0
+and not exists(
+select * from aca_Menu_x_seg_usuario as f
+where f.IdEmpresa = @IdEmpresa 
+and f.IdMenu = menu.IdMenuPadre
+and f.IdUsuario = @IdUsuario
+)
+group by menu.IdMenuPadre
+
+insert into aca_Menu_x_seg_usuario(IdEmpresa,IdSede,IdMenu,IdUsuario,Perfil)
+select @IdEmpresa, @IdSede, menu.IdMenuPadre, @IdUsuario, 0
+from seg_Menu as menu
+join aca_Menu_x_seg_usuario as me
+on me.IdMenu = menu.IdMenu
+where me.IdEmpresa = @IdEmpresa 
+and me.IdUsuario = @IdUsuario
+and menu.IdMenuPadre != 0
+and not exists(
+select * from aca_Menu_x_seg_usuario as f
+where f.IdEmpresa = @IdEmpresa 
+and f.IdMenu = menu.IdMenuPadre
+and f.IdUsuario = @IdUsuario
+)
+group by menu.IdMenuPadre

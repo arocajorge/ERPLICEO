43c730079032e3bc2ed8708aa1d510ba2a497acf
arocajorge <arocajorge92@gmail.com>
2019-11-14 14:53:48 -0500

    .

--- ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo_Rubro_Periodo.sql
+++ ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo_Rubro_Periodo.sql
@@ -1,12 +1,13 @@
 ﻿CREATE TABLE [dbo].[aca_AnioLectivo_Rubro_Periodo] (
-    [IdEmpresa]        INT  NOT NULL,
-    [IdAnio]           INT  NOT NULL,
-    [IdRubro]          INT  NOT NULL,
-    [IdPeriodo]        INT  NOT NULL,
-    [FechaFacturacion] DATE NULL,
-    [FechaPago]        DATE NULL,
+    [IdEmpresa]   INT         NOT NULL,
+    [IdAnio]      INT         NOT NULL,
+    [IdRubro]     INT         NOT NULL,
+    [IdPeriodo]   INT         NOT NULL,
+    [Observacion] VARCHAR (1) NULL,
     CONSTRAINT [PK_aca_AnioLectivo_Rubro_Periodo] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdAnio] ASC, [IdRubro] ASC, [IdPeriodo] ASC),
     CONSTRAINT [FK_aca_AnioLectivo_Rubro_Periodo_aca_AnioLectivo_Periodo] FOREIGN KEY ([IdEmpresa], [IdPeriodo]) REFERENCES [dbo].[aca_AnioLectivo_Periodo] ([IdEmpresa], [IdPeriodo]),
     CONSTRAINT [FK_aca_AnioLectivo_Rubro_Periodo_aca_AnioLectivo_Rubro] FOREIGN KEY ([IdEmpresa], [IdAnio], [IdRubro]) REFERENCES [dbo].[aca_AnioLectivo_Rubro] ([IdEmpresa], [IdAnio], [IdRubro])
 );
+
+
 
---
+++ ERP_database/ERP_database/dbo/Tables/aca_Matricula_Rubro.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [dbo].[aca_Matricula_Rubro] (
+    [IdEmpresa]          INT             NOT NULL,
+    [IdMatricula]        NUMERIC (18)    NOT NULL,
+    [IdPeriodo]          INT             NOT NULL,
+    [IdRubro]            INT             NOT NULL,
+    [IdProducto]         NUMERIC (18)    NOT NULL,
+    [Subtotal]           NUMERIC (18, 2) NOT NULL,
+    [IdCod_Impuesto_Iva] VARCHAR (50)    NOT NULL,
+    [Porcentaje]         NUMERIC (18, 2) NOT NULL,
+    [ValorIVA]           NUMERIC (18, 2) NOT NULL,
+    [Total]              NUMERIC (18, 2) NOT NULL,
+    [IdSucursal]         INT             NULL,
+    [IdBodega]           INT             NULL,
+    [IdCbteVta]          NUMERIC (18)    NULL,
+    [FechaFacturacion]   DATETIME        NULL,
+    CONSTRAINT [PK_aca_Matricula_Rubro] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdMatricula] ASC, [IdPeriodo] ASC, [IdRubro] ASC),
+    CONSTRAINT [FK_aca_Matricula_Rubro_aca_Matricula] FOREIGN KEY ([IdEmpresa], [IdMatricula]) REFERENCES [dbo].[aca_Matricula] ([IdEmpresa], [IdMatricula]),
+    CONSTRAINT [FK_aca_Matricula_Rubro_aca_Rubro] FOREIGN KEY ([IdEmpresa], [IdRubro]) REFERENCES [dbo].[aca_Rubro] ([IdEmpresa], [IdRubro]),
+    CONSTRAINT [FK_aca_Matricula_Rubro_fa_factura] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdBodega], [IdCbteVta]) REFERENCES [dbo].[fa_factura] ([IdEmpresa], [IdSucursal], [IdBodega], [IdCbteVta])
+);
+

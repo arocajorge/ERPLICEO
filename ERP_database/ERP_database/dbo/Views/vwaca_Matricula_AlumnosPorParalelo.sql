546e5352ef8c6ad4035d17372e7885bbf721f22b
arocajorge <arocajorge92@gmail.com>
2019-11-14 23:47:22 -0500

    1

---
+++ ERP_database/ERP_database/dbo/Views/vwaca_Matricula_AlumnosPorParalelo.sql
@@ -0,0 +1,7 @@
+﻿CREATE VIEW vwaca_Matricula_AlumnosPorParalelo
+AS
+select a.IdEmpresa, a.IdAnio, a.IdSede, a.IdNivel, a.IdJornada, a.IdCurso, a.IdParalelo, p.pe_nombreCompleto, p.pe_cedulaRuc, a.IdMatricula, a.Fecha
+from aca_Matricula as a inner join 
+aca_Alumno as b on a.IdEmpresa = b.IdEmpresa and a.IdAlumno = b.IdAlumno inner join
+tb_persona as p on b.IdPersona = p.IdPersona
+where b.Estado = 1

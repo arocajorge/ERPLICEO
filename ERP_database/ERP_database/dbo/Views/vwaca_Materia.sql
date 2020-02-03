0cc4a05b3d9e55d85bd6e357bcbc8459a94a1d0e
WendyCherres <wendycherres@gmail.com>
2020-02-03 15:41:44 -0500

    ...

--- ERP_database/ERP_database/dbo/Views/vwaca_Materia.sql
+++ ERP_database/ERP_database/dbo/Views/vwaca_Materia.sql
@@ -2,9 +2,9 @@
 AS
 SELECT dbo.aca_Materia.IdEmpresa, dbo.aca_Materia.IdMateria, dbo.aca_Materia.IdMateriaArea, dbo.aca_Materia.IdMateriaGrupo, dbo.aca_Materia.OrdenMateria, dbo.aca_MateriaArea.OrdenMateriaArea, 
                   dbo.aca_MateriaGrupo.OrdenMateriaGrupo, dbo.aca_Materia.NomMateria, dbo.aca_MateriaArea.NomMateriaArea, dbo.aca_MateriaGrupo.NomMateriaGrupo, dbo.aca_Materia.EsObligatorio, dbo.aca_Materia.Estado
-FROM     dbo.aca_Materia INNER JOIN
-                  dbo.aca_MateriaGrupo ON dbo.aca_Materia.IdEmpresa = dbo.aca_MateriaGrupo.IdEmpresa AND dbo.aca_Materia.IdMateriaGrupo = dbo.aca_MateriaGrupo.IdMateriaGrupo INNER JOIN
-                  dbo.aca_MateriaArea ON dbo.aca_Materia.IdEmpresa = dbo.aca_MateriaArea.IdEmpresa AND dbo.aca_Materia.IdMateriaArea = dbo.aca_MateriaArea.IdMateriaArea
+FROM     dbo.aca_Materia LEFT OUTER JOIN
+                  dbo.aca_MateriaArea ON dbo.aca_Materia.IdEmpresa = dbo.aca_MateriaArea.IdEmpresa AND dbo.aca_Materia.IdMateriaArea = dbo.aca_MateriaArea.IdMateriaArea LEFT OUTER JOIN
+                  dbo.aca_MateriaGrupo ON dbo.aca_Materia.IdEmpresa = dbo.aca_MateriaGrupo.IdEmpresa AND dbo.aca_Materia.IdMateriaGrupo = dbo.aca_MateriaGrupo.IdMateriaGrupo
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vwaca_Materia';
 
@@ -15,7 +15,7 @@ Begin DesignProperties =
    Begin PaneConfigurations = 
       Begin PaneConfiguration = 0
          NumPanes = 4
-         Configuration = "(H (1[21] 4[46] 2[8] 3) )"
+         Configuration = "(H (1[37] 4[31] 2[8] 3) )"
       End
       Begin PaneConfiguration = 1
          NumPanes = 3
@@ -94,9 +94,9 @@ Begin DesignProperties =
          Begin Table = "aca_MateriaGrupo"
             Begin Extent = 
                Top = 0
-               Left = 594
+               Left = 658
                Bottom = 163
-               Right = 839
+               Right = 903
             End
             DisplayFlags = 280
             TopColumn = 0
@@ -153,6 +153,8 @@ Begin DesignProperties =
    End
 End
 ', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vwaca_Materia';
+
+
 
 
 

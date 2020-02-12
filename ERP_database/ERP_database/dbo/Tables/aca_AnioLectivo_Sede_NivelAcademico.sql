a61dbc3ed2a16fdfd1581f11dcbf9092bda34c53
WendyCherres <wendycherres@gmail.com>
2020-02-12 10:51:01 -0500

    ,...

--- ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo_Sede_NivelAcademico.sql
+++ ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo_Sede_NivelAcademico.sql
@@ -1,13 +1,16 @@
 ﻿CREATE TABLE [dbo].[aca_AnioLectivo_Sede_NivelAcademico] (
-    [IdEmpresa] INT           NOT NULL,
-    [IdAnio]    INT           NOT NULL,
-    [IdSede]    INT           NOT NULL,
-    [IdNivel]   INT           NOT NULL,
-    [NomSede]   VARCHAR (500) NOT NULL,
-    [NomNivel]  VARCHAR (500) NOT NULL,
+    [IdEmpresa]  INT           NOT NULL,
+    [IdAnio]     INT           NOT NULL,
+    [IdSede]     INT           NOT NULL,
+    [IdNivel]    INT           NOT NULL,
+    [NomSede]    VARCHAR (500) NOT NULL,
+    [NomNivel]   VARCHAR (500) NOT NULL,
+    [OrdenNivel] INT           NOT NULL,
     CONSTRAINT [PK_aca_AnioLectivo_Sede_NivelAcademico] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdAnio] ASC, [IdSede] ASC, [IdNivel] ASC),
     CONSTRAINT [FK_aca_AnioLectivo_Sede_NivelAcademico_aca_AnioLectivo] FOREIGN KEY ([IdEmpresa], [IdAnio]) REFERENCES [dbo].[aca_AnioLectivo] ([IdEmpresa], [IdAnio]),
     CONSTRAINT [FK_aca_AnioLectivo_Sede_NivelAcademico_aca_NivelAcademico] FOREIGN KEY ([IdEmpresa], [IdNivel]) REFERENCES [dbo].[aca_NivelAcademico] ([IdEmpresa], [IdNivel]),
     CONSTRAINT [FK_aca_AnioLectivo_Sede_NivelAcademico_aca_Sede] FOREIGN KEY ([IdEmpresa], [IdSede]) REFERENCES [dbo].[aca_Sede] ([IdEmpresa], [IdSede])
 );
+
+
 

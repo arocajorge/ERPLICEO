77bd37a27aa525323a69bd6fc375da20c43e8f11
arocajorge <arocajorge92@gmail.com>
2020-03-09 14:54:14 -0500

    11

--- ERP_database/ERP_database/dbo/Tables/aca_Matricula.sql
+++ ERP_database/ERP_database/dbo/Tables/aca_Matricula.sql
@@ -22,12 +22,17 @@
     [IdUsuarioAnulacion]    VARCHAR (200) NULL,
     [FechaAnulacion]        DATETIME      NULL,
     [MotivoAnulacion]       VARCHAR (MAX) NULL,
+    [IdEmpresa_rol]         INT           NULL,
+    [IdEmpleado]            NUMERIC (18)  NULL,
     CONSTRAINT [PK_aca_Matricula] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdMatricula] ASC),
     CONSTRAINT [FK_aca_Matricula_aca_alumno] FOREIGN KEY ([IdEmpresa], [IdAlumno]) REFERENCES [dbo].[aca_Alumno] ([IdEmpresa], [IdAlumno]),
     CONSTRAINT [FK_aca_Matricula_aca_AnioLectivo_Curso_Paralelo] FOREIGN KEY ([IdEmpresa], [IdAnio], [IdSede], [IdNivel], [IdJornada], [IdCurso], [IdParalelo]) REFERENCES [dbo].[aca_AnioLectivo_Curso_Paralelo] ([IdEmpresa], [IdAnio], [IdSede], [IdNivel], [IdJornada], [IdCurso], [IdParalelo]),
     CONSTRAINT [FK_aca_Matricula_aca_MecanismoDePago] FOREIGN KEY ([IdEmpresa], [IdMecanismo]) REFERENCES [dbo].[aca_MecanismoDePago] ([IdEmpresa], [IdMecanismo]),
-    CONSTRAINT [FK_aca_Matricula_aca_Plantilla] FOREIGN KEY ([IdEmpresa], [IdAnio], [IdPlantilla]) REFERENCES [dbo].[aca_Plantilla] ([IdEmpresa], [IdAnio], [IdPlantilla])
+    CONSTRAINT [FK_aca_Matricula_aca_Plantilla] FOREIGN KEY ([IdEmpresa], [IdAnio], [IdPlantilla]) REFERENCES [dbo].[aca_Plantilla] ([IdEmpresa], [IdAnio], [IdPlantilla]),
+    CONSTRAINT [FK_aca_Matricula_ro_empleado] FOREIGN KEY ([IdEmpresa_rol], [IdEmpleado]) REFERENCES [dbo].[ro_empleado] ([IdEmpresa], [IdEmpleado])
 );
+
+
 
 
 

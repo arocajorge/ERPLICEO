372cd24c6734fbee9f29716ae8f0aa471c7edc14
WendyCherres <wendycherres@gmail.com>
2020-04-02 10:39:15 -0500

    ..

--- ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo.sql
+++ ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo.sql
@@ -1,22 +1,26 @@
 ﻿CREATE TABLE [dbo].[aca_AnioLectivo] (
-    [IdEmpresa]             INT           NOT NULL,
-    [IdAnio]                INT           NOT NULL,
-    [Descripcion]           VARCHAR (50)  NOT NULL,
-    [FechaDesde]            DATE          NOT NULL,
-    [FechaHasta]            DATE          NOT NULL,
-    [Estado]                BIT           NOT NULL,
-    [EnCurso]               BIT           NOT NULL,
-    [BloquearMatricula]     BIT           NOT NULL,
-    [IdAnioLectivoAnterior] INT           NULL,
-    [IdUsuarioCreacion]     VARCHAR (200) NULL,
-    [FechaCreacion]         DATETIME      NULL,
-    [IdUsuarioModificacion] VARCHAR (200) NULL,
-    [FechaModificacion]     DATETIME      NULL,
-    [IdUsuarioAnulacion]    VARCHAR (200) NULL,
-    [FechaAnulacion]        DATETIME      NULL,
-    [MotivoAnulacion]       VARCHAR (MAX) NULL,
+    [IdEmpresa]               INT           NOT NULL,
+    [IdAnio]                  INT           NOT NULL,
+    [Descripcion]             VARCHAR (50)  NOT NULL,
+    [FechaDesde]              DATE          NOT NULL,
+    [FechaHasta]              DATE          NOT NULL,
+    [Estado]                  BIT           NOT NULL,
+    [EnCurso]                 BIT           NOT NULL,
+    [BloquearMatricula]       BIT           NOT NULL,
+    [IdAnioLectivoAnterior]   INT           NULL,
+    [PromedioMinimoParcial]   FLOAT (53)    NULL,
+    [PromedioMinimoPromocion] FLOAT (53)    NULL,
+    [IdUsuarioCreacion]       VARCHAR (200) NULL,
+    [FechaCreacion]           DATETIME      NULL,
+    [IdUsuarioModificacion]   VARCHAR (200) NULL,
+    [FechaModificacion]       DATETIME      NULL,
+    [IdUsuarioAnulacion]      VARCHAR (200) NULL,
+    [FechaAnulacion]          DATETIME      NULL,
+    [MotivoAnulacion]         VARCHAR (MAX) NULL,
     CONSTRAINT [PK_aca_AnioLectivo] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdAnio] ASC)
 );
+
+
 
 
 
--- ERP_database/ERP_database/dbo/Tables/aca_Parametro.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[aca_Parametro] (
-    [IdEmpresa]               INT        NOT NULL,
-    [PromedioMinimoParcial]   FLOAT (53) NULL,
-    [PromedioMinimoPromocion] FLOAT (53) NULL,
-    CONSTRAINT [PK_aca_Parametro] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC)
-);
-

f257445e5cf5c3c96b1e019db37214110ed70de5
arocajorge <arocajorge92@gmail.com>
2020-04-16 21:54:17 -0500

    2

---
+++ ERP_database/ERP_database/Academico/Stored Procedures/SPCXC_007.sql
@@ -0,0 +1,49 @@
+﻿--EXEC Academico.SPFAC_007 1 ,'2020/4/20'
+CREATE PROCEDURE Academico.SPCXC_007
+(
+@IdEmpresa int,
+@FechaCorte date
+)
+AS
+
+select a.IdEmpresa, a.IdSucursal, a.IdBodega, a.IdCbteVta, a.vt_fecha, a.vt_Observacion, A.vt_serie1+'-'+A.vt_serie2+'-'+ A.vt_NumFactura vt_NumFactura, c.IdAlumno, c.Codigo as CodigoAlumno, d.pe_nombreCompleto, b.IdAnio, year(isnull(e.FechaDesde,a.vt_fecha)) as Periodo, b.Total, isnull(f.dc_ValorPago,0) as TotalPagado, round(b.Total - isnull(f.dc_ValorPago,0),2) as Saldo,
+a.vt_fech_venc, DATEDIFF(day,a.vt_fech_venc,@FechaCorte) as Plazo,
+CASE WHEN DATEDIFF(day,a.vt_fech_venc,@FechaCorte) <= 30 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_0_30,
+CASE WHEN DATEDIFF(day,a.vt_fech_venc,@FechaCorte) BETWEEN 31 AND 60 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_31_60,
+CASE WHEN DATEDIFF(day,a.vt_fech_venc,@FechaCorte) BETWEEN 61 AND 90 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_61_90,
+CASE WHEN DATEDIFF(day,a.vt_fech_venc,@FechaCorte) BETWEEN 91 AND 180 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_91_180,
+CASE WHEN DATEDIFF(day,a.vt_fech_venc,@FechaCorte) >= 181 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_181
+from fa_factura as a inner join
+fa_factura_resumen as b on a.IdEmpresa = b.IdEmpresa and a.IdSucursal = b.IdSucursal and a.IdBodega = b.IdBodega and a.IdCbteVta = b.IdCbteVta inner join
+aca_Alumno as c on a.IdEmpresa = c.IdEmpresa and a.IdAlumno = c.IdAlumno inner join
+tb_persona as d on c.IdPersona = d.IdPersona left join
+aca_AnioLectivo as e on b.IdEmpresa = e.IdEmpresa and b.IdAnio = e.IdAnio left join
+(
+	select B.IdEmpresa, B.IdSucursal, B.IdBodega_Cbte, B.IdCbte_vta_nota, B.dc_TipoDocumento, SUM(B.dc_ValorPago) dc_ValorPago 
+	from cxc_cobro as a inner join 
+	cxc_cobro_det as b on a.IdEmpresa = b.IdEmpresa and a.IdSucursal = b.IdSucursal and a.IdCobro = b.IdCobro
+	where a.cr_estado = 'A' and a.IdEmpresa = @IdEmpresa and a.cr_fecha <= @FechaCorte
+	group by B.IdEmpresa, B.IdSucursal, B.IdBodega_Cbte, B.IdCbte_vta_nota, B.dc_TipoDocumento
+) as f on a.IdEmpresa = f.IdEmpresa and a.IdSucursal = f.IdSucursal and a.IdBodega = f.IdBodega_Cbte and a.IdCbteVta = f.IdCbte_vta_nota and a.vt_tipoDoc = f.dc_TipoDocumento
+where a.IdEmpresa = @IdEmpresa and a.Estado = 'A' AND a.vt_fecha <= @FechaCorte and round(b.Total - isnull(f.dc_ValorPago,0),2) > 0
+UNION ALL
+select a.IdEmpresa, a.IdSucursal, a.IdBodega, a.IdNota, a.no_fecha, a.sc_observacion, CASE WHEN A.NaturalezaNota = 'SRI' THEN A.Serie1+'-'+A.Serie2+'-'+ A.NumNota_Impresa ELSE ISNULL(A.CodNota,cast(A.IdNota as varchar)) END NumNota_Impresa  ,c.IdAlumno, c.Codigo as CodigoAlumno, d.pe_nombreCompleto, b.IdAnio, year(isnull(e.FechaDesde,a.no_fecha)) as Periodo, b.Total, isnull(f.dc_ValorPago,0) as TotalPagado, round(b.Total - isnull(f.dc_ValorPago,0),2) as Saldo,
+a.no_fecha_venc, DATEDIFF(day,a.no_fecha_venc,@FechaCorte) as Plazo,
+CASE WHEN DATEDIFF(day,a.no_fecha_venc,@FechaCorte) <= 30 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_0_30,
+CASE WHEN DATEDIFF(day,a.no_fecha_venc,@FechaCorte) BETWEEN 31 AND 60 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_31_60,
+CASE WHEN DATEDIFF(day,a.no_fecha_venc,@FechaCorte) BETWEEN 61 AND 90 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_61_90,
+CASE WHEN DATEDIFF(day,a.no_fecha_venc,@FechaCorte) BETWEEN 91 AND 180 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_91_180,
+CASE WHEN DATEDIFF(day,a.no_fecha_venc,@FechaCorte) >= 181 THEN round(b.Total - isnull(f.dc_ValorPago,0),2) ELSE 0 END AS VENCIDO_181
+from fa_notaCreDeb as a inner join
+fa_notaCreDeb_resumen as b on a.IdEmpresa = b.IdEmpresa and a.IdSucursal = b.IdSucursal and a.IdBodega = b.IdBodega and a.IdNota = b.IdNota inner join
+aca_Alumno as c on a.IdEmpresa = c.IdEmpresa and a.IdAlumno = c.IdAlumno inner join
+tb_persona as d on c.IdPersona = d.IdPersona left join
+aca_AnioLectivo as e on b.IdEmpresa = e.IdEmpresa and b.IdAnio = e.IdAnio left join
+(
+	select B.IdEmpresa, B.IdSucursal, B.IdBodega_Cbte, B.IdCbte_vta_nota, B.dc_TipoDocumento, SUM(B.dc_ValorPago) dc_ValorPago 
+	from cxc_cobro as a inner join 
+	cxc_cobro_det as b on a.IdEmpresa = b.IdEmpresa and a.IdSucursal = b.IdSucursal and a.IdCobro = b.IdCobro
+	where a.cr_estado = 'A' and a.IdEmpresa = @IdEmpresa and a.cr_fecha <= @FechaCorte
+	group by B.IdEmpresa, B.IdSucursal, B.IdBodega_Cbte, B.IdCbte_vta_nota, B.dc_TipoDocumento
+) as f on a.IdEmpresa = f.IdEmpresa and a.IdSucursal = f.IdSucursal and a.IdBodega = f.IdBodega_Cbte and a.IdNota = f.IdCbte_vta_nota and a.CodDocumentoTipo = f.dc_TipoDocumento
+where a.IdEmpresa = @IdEmpresa and a.Estado = 'A' AND a.no_fecha <= @FechaCorte and round(b.Total - isnull(f.dc_ValorPago,0),2) > 0 and a.CreDeb = 'D'
---
+++ ERP_database/ERP_database/Academico/Views/VWACA_012.sql
@@ -0,0 +1,22 @@
+﻿CREATE VIEW Academico.VWACA_012
+AS
+SELECT a.IdEmpresa, c.IdSucursal, c.IdBodega, c.IdCbteVta, b.IdPeriodo, b.IdRubro, a.IdAlumno, a.IdAnio, a.IdSede, a.IdNivel, a.IdJornada, a.IdCurso, a.IdParalelo, c.vt_serie1+'-'+ c.vt_serie2+'-'+ c.vt_NumFactura vt_NumFactura, c.vt_fecha, c.vt_Observacion, 
+                  cxc_cobro.cr_fecha, cxc_cobro_det.dc_ValorPago, sn.NomSede, sn.OrdenNivel, sn.NomNivel, nj.OrdenJornada, nj.NomJornada, jc.OrdenCurso, jc.NomCurso, cp.OrdenParalelo, cp.NomParalelo, d.Codigo as CodigoAlumno,
+				  e.pe_cedulaRuc, e.pe_nombreCompleto, e.pe_fechaNacimiento, h.Nacionalidad, i.NomRubro, j.Descripcion as DescripcionAnio
+FROM     aca_AnioLectivo_Sede_NivelAcademico AS sn RIGHT OUTER JOIN
+                  aca_AnioLectivo_NivelAcademico_Jornada AS nj ON sn.IdEmpresa = nj.IdEmpresa AND sn.IdAnio = nj.IdAnio AND sn.IdSede = nj.IdSede AND sn.IdNivel = nj.IdNivel RIGHT OUTER JOIN
+                  aca_AnioLectivo_Jornada_Curso AS jc ON nj.IdEmpresa = jc.IdEmpresa AND nj.IdAnio = jc.IdAnio AND nj.IdSede = jc.IdSede AND nj.IdNivel = jc.IdNivel AND nj.IdJornada = jc.IdJornada RIGHT OUTER JOIN
+                  aca_AnioLectivo_Curso_Paralelo AS cp ON jc.IdEmpresa = cp.IdEmpresa AND jc.IdAnio = cp.IdAnio AND jc.IdSede = cp.IdSede AND jc.IdNivel = cp.IdNivel AND jc.IdJornada = cp.IdJornada AND jc.IdCurso = cp.IdCurso RIGHT OUTER JOIN
+                  cxc_cobro_det INNER JOIN
+                  cxc_cobro ON cxc_cobro_det.IdEmpresa = cxc_cobro.IdEmpresa AND cxc_cobro_det.IdSucursal = cxc_cobro.IdSucursal AND cxc_cobro_det.IdCobro = cxc_cobro.IdCobro INNER JOIN
+                  fa_factura AS c ON cxc_cobro_det.IdEmpresa = c.IdEmpresa AND cxc_cobro_det.IdSucursal = c.IdSucursal AND cxc_cobro_det.IdBodega_Cbte = c.IdBodega AND cxc_cobro_det.IdCbte_vta_nota = c.IdCbteVta AND 
+                  cxc_cobro_det.dc_TipoDocumento = c.vt_tipoDoc RIGHT OUTER JOIN
+                  aca_Matricula AS a INNER JOIN
+                  aca_Matricula_Rubro AS b ON a.IdEmpresa = b.IdEmpresa AND a.IdMatricula = b.IdMatricula ON c.IdEmpresa = b.IdEmpresa AND c.IdSucursal = b.IdSucursal AND c.IdBodega = b.IdBodega AND c.IdCbteVta = b.IdCbteVta ON 
+                  cp.IdEmpresa = a.IdEmpresa AND cp.IdAnio = a.IdAnio AND cp.IdSede = a.IdSede AND cp.IdNivel = a.IdNivel AND cp.IdJornada = a.IdJornada AND cp.IdCurso = a.IdCurso AND cp.IdParalelo = a.IdParalelo inner join
+				  aca_Alumno as d on d.IdEmpresa = a.IdEmpresa and d.IdAlumno = a.IdAlumno inner join
+				  tb_persona as e on d.IdPersona = e.IdPersona left join
+				  tb_pais as h on d.IdPais = h.IdPais left join 
+				  aca_AnioLectivo_Rubro as i on b.IdEmpresa = i.IdEmpresa and b.IdAnio = i.IdAnio and b.IdRubro = i.IdRubro left join
+				  aca_AnioLectivo as j on a.IdEmpresa = j.IdEmpresa and a.IdAnio = j.IdAnio
+where cxc_cobro.cr_estado = 'A'

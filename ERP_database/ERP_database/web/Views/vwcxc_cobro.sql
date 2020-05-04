4350ceeed94eabc9461e8bc6042da892b49fe3f8
arocajorge <arocajorge92@gmail.com>
2020-05-04 09:36:47 -0500

    .

--- ERP_database/ERP_database/web/Views/vwcxc_cobro.sql
+++ ERP_database/ERP_database/web/Views/vwcxc_cobro.sql
@@ -1,24 +1,26 @@
 ﻿CREATE VIEW [web].[vwcxc_cobro]
 AS
-SELECT dbo.cxc_cobro.IdEmpresa, dbo.cxc_cobro.IdSucursal, dbo.cxc_cobro.IdCobro, dbo.cxc_cobro.IdCliente, dbo.tb_persona.pe_nombreCompleto, dbo.cxc_cobro.IdCobro_tipo, dbo.cxc_cobro.cr_fecha, dbo.cxc_cobro.cr_TotalCobro, 
-                  dbo.cxc_cobro.cr_estado, dbo.tb_sucursal.Su_Descripcion, dbo.cxc_cobro.cr_observacion, isnull(dbo.cxc_cobro.cr_NumDocumento,ba.ba_descripcion) cr_NumDocumento, ISNULL(Tipo.nom_Motivo_tipo_cobro, t2.nom_Motivo_tipo_cobro) AS nom_Motivo_tipo_cobro, 
-                  dbo.cxc_cobro.IdAlumno, tb_persona_1.pe_nombreCompleto AS NomAlumno, dbo.cxc_cobro.cr_Saldo, dbo.cxc_cobro.IdUsuario
-FROM     dbo.tb_persona AS tb_persona_1 INNER JOIN
-                  dbo.aca_Alumno ON tb_persona_1.IdPersona = dbo.aca_Alumno.IdPersona RIGHT OUTER JOIN
-                  dbo.cxc_cobro INNER JOIN
-                  dbo.fa_cliente ON dbo.cxc_cobro.IdEmpresa = dbo.fa_cliente.IdEmpresa AND dbo.cxc_cobro.IdCliente = dbo.fa_cliente.IdCliente INNER JOIN
-                  dbo.tb_persona ON dbo.fa_cliente.IdPersona = dbo.tb_persona.IdPersona INNER JOIN
-                  dbo.tb_sucursal ON dbo.cxc_cobro.IdEmpresa = dbo.tb_sucursal.IdEmpresa AND dbo.cxc_cobro.IdSucursal = dbo.tb_sucursal.IdSucursal ON dbo.aca_Alumno.IdEmpresa = dbo.cxc_cobro.IdEmpresa AND 
-                  dbo.aca_Alumno.IdAlumno = dbo.cxc_cobro.IdAlumno LEFT OUTER JOIN
-                      (SELECT dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdCobro, dbo.cxc_cobro_tipo_motivo.nom_Motivo_tipo_cobro
-                       FROM      dbo.cxc_cobro_det INNER JOIN
-                                         dbo.cxc_cobro_tipo ON dbo.cxc_cobro_det.IdCobro_tipo = dbo.cxc_cobro_tipo.IdCobro_tipo INNER JOIN
-                                         dbo.cxc_cobro_tipo_motivo ON dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro = dbo.cxc_cobro_tipo_motivo.IdMotivo_tipo_cobro
-                       GROUP BY dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdCobro, dbo.cxc_cobro_tipo_motivo.nom_Motivo_tipo_cobro) AS Tipo ON Tipo.IdEmpresa = dbo.cxc_cobro.IdEmpresa AND 
-                  Tipo.IdSucursal = dbo.cxc_cobro.IdSucursal AND Tipo.IdCobro = dbo.cxc_cobro.IdCobro LEFT OUTER JOIN
-                  dbo.cxc_cobro_tipo AS t1 ON dbo.cxc_cobro.IdCobro_tipo = t1.IdCobro_tipo LEFT OUTER JOIN
-                  dbo.cxc_cobro_tipo_motivo AS t2 ON t1.IdMotivo_tipo_cobro = t2.IdMotivo_tipo_cobro left join
-				  ba_Banco_Cuenta as ba on cxc_cobro.IdEmpresa = ba.IdEmpresa and cxc_cobro.IdBanco = ba.IdBanco
+SELECT     dbo.cxc_cobro.IdEmpresa, dbo.cxc_cobro.IdSucursal, dbo.cxc_cobro.IdCobro, dbo.cxc_cobro.IdCliente, dbo.tb_persona.pe_nombreCompleto, 
+                      dbo.cxc_cobro.IdCobro_tipo, dbo.cxc_cobro.cr_fecha, dbo.cxc_cobro.cr_TotalCobro, dbo.cxc_cobro.cr_estado, dbo.tb_sucursal.Su_Descripcion, 
+                      dbo.cxc_cobro.cr_observacion, ISNULL(ISNULL(cxc_cobro.CR_TARJETA, dbo.cxc_cobro.cr_NumDocumento), ba.ba_descripcion) AS cr_NumDocumento, ISNULL(Tipo.nom_Motivo_tipo_cobro, 
+                      t2.nom_Motivo_tipo_cobro) AS nom_Motivo_tipo_cobro, dbo.cxc_cobro.IdAlumno, tb_persona_1.pe_nombreCompleto AS NomAlumno, dbo.cxc_cobro.cr_Saldo, 
+                      dbo.cxc_cobro.IdUsuario
+FROM         dbo.tb_persona AS tb_persona_1 INNER JOIN
+                      dbo.aca_Alumno ON tb_persona_1.IdPersona = dbo.aca_Alumno.IdPersona RIGHT OUTER JOIN
+                      dbo.cxc_cobro INNER JOIN
+                      dbo.fa_cliente ON dbo.cxc_cobro.IdEmpresa = dbo.fa_cliente.IdEmpresa AND dbo.cxc_cobro.IdCliente = dbo.fa_cliente.IdCliente INNER JOIN
+                      dbo.tb_persona ON dbo.fa_cliente.IdPersona = dbo.tb_persona.IdPersona INNER JOIN
+                      dbo.tb_sucursal ON dbo.cxc_cobro.IdEmpresa = dbo.tb_sucursal.IdEmpresa AND dbo.cxc_cobro.IdSucursal = dbo.tb_sucursal.IdSucursal ON 
+                      dbo.aca_Alumno.IdEmpresa = dbo.cxc_cobro.IdEmpresa AND dbo.aca_Alumno.IdAlumno = dbo.cxc_cobro.IdAlumno LEFT OUTER JOIN
+                          (SELECT     dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdCobro, dbo.cxc_cobro_tipo_motivo.nom_Motivo_tipo_cobro
+                            FROM          dbo.cxc_cobro_det INNER JOIN
+                                                   dbo.cxc_cobro_tipo ON dbo.cxc_cobro_det.IdCobro_tipo = dbo.cxc_cobro_tipo.IdCobro_tipo INNER JOIN
+                                                   dbo.cxc_cobro_tipo_motivo ON dbo.cxc_cobro_tipo.IdMotivo_tipo_cobro = dbo.cxc_cobro_tipo_motivo.IdMotivo_tipo_cobro
+                            GROUP BY dbo.cxc_cobro_det.IdEmpresa, dbo.cxc_cobro_det.IdSucursal, dbo.cxc_cobro_det.IdCobro, dbo.cxc_cobro_tipo_motivo.nom_Motivo_tipo_cobro) 
+                      AS Tipo ON Tipo.IdEmpresa = dbo.cxc_cobro.IdEmpresa AND Tipo.IdSucursal = dbo.cxc_cobro.IdSucursal AND Tipo.IdCobro = dbo.cxc_cobro.IdCobro LEFT OUTER JOIN
+                      dbo.cxc_cobro_tipo AS t1 ON dbo.cxc_cobro.IdCobro_tipo = t1.IdCobro_tipo LEFT OUTER JOIN
+                      dbo.cxc_cobro_tipo_motivo AS t2 ON t1.IdMotivo_tipo_cobro = t2.IdMotivo_tipo_cobro LEFT OUTER JOIN
+                      dbo.ba_Banco_Cuenta AS ba ON dbo.cxc_cobro.IdEmpresa = ba.IdEmpresa AND dbo.cxc_cobro.IdBanco = ba.IdBanco
 
 
 GO

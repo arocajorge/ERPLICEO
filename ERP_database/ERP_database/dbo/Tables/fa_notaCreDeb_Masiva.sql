60c9ebdd89b899f77c88b092618cc49c3b99ef28
arocajorge <arocajorge92@gmail.com>
2020-06-04 09:32:34 -0500

    .

---
+++ ERP_database/ERP_database/dbo/Tables/fa_notaCreDeb_Masiva.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [dbo].[fa_notaCreDeb_Masiva] (
+    [IdEmpresa]             INT           NOT NULL,
+    [IdNCMasivo]            NUMERIC (18)  NOT NULL,
+    [IdSucursal]            INT           NOT NULL,
+    [IdBodega]              INT           NOT NULL,
+    [IdPuntoVta]            INT           NOT NULL,
+    [CreDeb]                VARCHAR (1)   NOT NULL,
+    [no_fecha]              DATETIME      NOT NULL,
+    [no_fecha_venc]         DATETIME      NOT NULL,
+    [IdTipoNota]            INT           NOT NULL,
+    [NaturalezaNota]        CHAR (3)      NULL,
+    [sc_observacion]        VARCHAR (MAX) NULL,
+    [Estado]                BIT           NOT NULL,
+    [IdUsuarioCreacion]     VARCHAR (200) NULL,
+    [FechaCreacion]         DATETIME      NULL,
+    [IdUsuarioModificacion] VARCHAR (200) NULL,
+    [FechaModificacion]     DATETIME      NULL,
+    [IdUsuarioAnulacion]    VARCHAR (200) NULL,
+    [FechaAnulacion]        DATETIME      NULL,
+    [MotivoAnulacion]       VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_fa_notaCreDeb_Masiva] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdNCMasivo] ASC),
+    CONSTRAINT [FK_fa_notaCreDeb_Masiva_fa_TipoNota] FOREIGN KEY ([IdEmpresa], [IdTipoNota]) REFERENCES [dbo].[fa_TipoNota] ([IdEmpresa], [IdTipoNota])
+);
+
---
+++ ERP_database/ERP_database/dbo/Tables/fa_notaCreDeb_MasivaDet.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[fa_notaCreDeb_MasivaDet] (
+    [IdEmpresa]          INT           NOT NULL,
+    [IdNCMasivo]         NUMERIC (18)  NOT NULL,
+    [Secuencia]          INT           NOT NULL,
+    [IdAlumno]           NUMERIC (18)  NOT NULL,
+    [Subtotal]           FLOAT (53)    NOT NULL,
+    [IVA]                FLOAT (53)    NOT NULL,
+    [vt_por_iva]         FLOAT (53)    NOT NULL,
+    [IdCod_Impuesto_Iva] VARCHAR (25)  NOT NULL,
+    [Total]              FLOAT (53)    NOT NULL,
+    [ObservacionDet]     VARCHAR (MAX) NULL,
+    [IdSucursal]         INT           NULL,
+    [IdBodega]           INT           NULL,
+    [IdNota]             NUMERIC (18)  NULL,
+    CONSTRAINT [PK_fa_notaCreDeb_MasivaDet] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdNCMasivo] ASC, [Secuencia] ASC),
+    CONSTRAINT [FK_fa_notaCreDeb_MasivaDet_aca_Alumno] FOREIGN KEY ([IdEmpresa], [IdAlumno]) REFERENCES [dbo].[aca_Alumno] ([IdEmpresa], [IdAlumno]),
+    CONSTRAINT [FK_fa_notaCreDeb_MasivaDet_fa_notaCreDeb] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdBodega], [IdNota]) REFERENCES [dbo].[fa_notaCreDeb] ([IdEmpresa], [IdSucursal], [IdBodega], [IdNota])
+);
+

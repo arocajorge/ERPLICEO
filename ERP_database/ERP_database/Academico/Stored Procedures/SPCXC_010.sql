58a83e801d91297cd6207f3fc9ee1d59a78761c0
WendyCherres <wendycherres@gmail.com>
2020-06-23 16:39:25 -0500

    ..

--- ERP_database/ERP_database/Academico/Stored Procedures/SPCXC_010.sql
+++ ERP_database/ERP_database/Academico/Stored Procedures/SPCXC_010.sql
@@ -1,5 +1,5 @@
 ﻿--exec Academico.SPCXC_010 1,4642,'2020/01/01','2020/05/31'
-CREATE PROCEDURE Academico.SPCXC_010
+CREATE PROCEDURE [Academico].[SPCXC_010]
 (
 @IdEmpresa int,
 @IdAlumno numeric,
@@ -12,6 +12,6 @@ DECLARE @SaldoInicial float
 
 SELECT @SaldoInicial = sum(Valor) FROM Academico.VWCXC_010 where IdEmpresa = @IdEmpresa and IdAlumno = @IdAlumno and vt_fecha < @FechaIni
 
-select IdEmpresa,Tipo,IdCbteVta,Orden,vt_fecha,vt_Observacion,Referencia,Anio,IdMes,smes,@SaldoInicial SaldoInicial, Debe,Haber,Valor, ISNULL(@SaldoInicial,0) + sum(Valor) over(order by vt_fecha, Orden, IdCbteVta),
+select IdEmpresa,Tipo,IdCbteVta,Orden,vt_fecha,vt_Observacion,Referencia,Anio,IdMes,smes,isnull(@SaldoInicial,0) SaldoInicial, Debe,Haber,Valor, ISNULL(@SaldoInicial,0) + sum(Valor) over(order by vt_fecha, Orden, IdCbteVta) Saldo,
 pe_nombreCompleto,CodigoAlumno,IdAlumno 
 from Academico.VWCXC_010 where IdEmpresa = @IdEmpresa AND IdAlumno = @IdAlumno AND vt_fecha BETWEEN @FechaIni AND @FechaFin

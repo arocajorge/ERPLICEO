9b2102593434aaabce9d132ffbfbab7bb3f60c37
arocajorge <arocajorge92@gmail.com>
2020-06-25 12:33:18 -0500

    asasa

--- ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo_Periodo.sql
+++ ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo_Periodo.sql
@@ -1,22 +1,31 @@
 ﻿CREATE TABLE [dbo].[aca_AnioLectivo_Periodo] (
-    [IdEmpresa]             INT           NOT NULL,
-    [IdPeriodo]             INT           NOT NULL,
-    [IdAnio]                INT           NOT NULL,
-    [IdMes]                 INT           NOT NULL,
-    [FechaDesde]            DATE          NOT NULL,
-    [FechaHasta]            DATE          NOT NULL,
-    [FechaProntoPago]       DATETIME      NULL,
-    [Estado]                BIT           NOT NULL,
-    [IdUsuarioCreacion]     VARCHAR (200) NULL,
-    [FechaCreacion]         DATETIME      NULL,
-    [IdUsuarioModificacion] VARCHAR (200) NULL,
-    [FechaModificacion]     DATETIME      NULL,
-    [IdUsuarioAnulacion]    VARCHAR (200) NULL,
-    [FechaAnulacion]        DATETIME      NULL,
-    [MotivoAnulacion]       VARCHAR (MAX) NULL,
+    [IdEmpresa]             INT             NOT NULL,
+    [IdPeriodo]             INT             NOT NULL,
+    [IdAnio]                INT             NOT NULL,
+    [IdMes]                 INT             NOT NULL,
+    [FechaDesde]            DATE            NOT NULL,
+    [FechaHasta]            DATE            NOT NULL,
+    [FechaProntoPago]       DATETIME        NULL,
+    [Estado]                BIT             NOT NULL,
+    [IdUsuarioCreacion]     VARCHAR (200)   NULL,
+    [FechaCreacion]         DATETIME        NULL,
+    [IdUsuarioModificacion] VARCHAR (200)   NULL,
+    [FechaModificacion]     DATETIME        NULL,
+    [IdUsuarioAnulacion]    VARCHAR (200)   NULL,
+    [FechaAnulacion]        DATETIME        NULL,
+    [MotivoAnulacion]       VARCHAR (MAX)   NULL,
+    [IdSucursal]            INT             NULL,
+    [IdPuntoVta]            INT             NULL,
+    [Procesado]             BIT             NULL,
+    [FechaProceso]          DATE            NULL,
+    [TotalAlumnos]          NUMERIC (18, 2) NULL,
+    [TotalValorFacturado]   NUMERIC (18, 2) NULL,
     CONSTRAINT [PK_aca_AnioLectivo_Periodo] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdPeriodo] ASC),
-    CONSTRAINT [FK_aca_AnioLectivo_Periodo_aca_AnioLectivo] FOREIGN KEY ([IdEmpresa], [IdAnio]) REFERENCES [dbo].[aca_AnioLectivo] ([IdEmpresa], [IdAnio])
+    CONSTRAINT [FK_aca_AnioLectivo_Periodo_aca_AnioLectivo] FOREIGN KEY ([IdEmpresa], [IdAnio]) REFERENCES [dbo].[aca_AnioLectivo] ([IdEmpresa], [IdAnio]),
+    CONSTRAINT [FK_aca_AnioLectivo_Periodo_fa_PuntoVta] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdPuntoVta]) REFERENCES [dbo].[fa_PuntoVta] ([IdEmpresa], [IdSucursal], [IdPuntoVta])
 );
+
+
 
 
 
---
+++ ERP_database/ERP_database/dbo/Tables/aca_AnioLectivo_PeriodoDet.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[aca_AnioLectivo_PeriodoDet] (
+    [IdEmpresa] INT NOT NULL,
+    [IdPeriodo] INT NOT NULL,
+    [Secuencia] INT NOT NULL,
+    CONSTRAINT [PK_aca_AnioLectivo_PeriodoDet] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdPeriodo] ASC, [Secuencia] ASC)
+);
+

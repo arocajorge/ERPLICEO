4783861e444e6fe87944cbcb9613fa54ebb3753e
arocajorge <arocajorge92@gmail.com>
2020-07-02 11:28:05 -0500

    ..

---
+++ ERP_database/ERP_database/dbo/Tables/tb_ColaCorreo.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tb_ColaCorreo] (
+    [IdEmpresa]         INT            NOT NULL,
+    [IdCorreo]          NUMERIC (18)   NOT NULL,
+    [Codigo]            VARCHAR (50)   NOT NULL,
+    [Destinatarios]     VARCHAR (1000) NOT NULL,
+    [Asunto]            VARCHAR (1000) NOT NULL,
+    [Cuerpo]            VARCHAR (MAX)  NOT NULL,
+    [Parametros]        VARCHAR (200)  NOT NULL,
+    [IdUsuarioCreacion] VARCHAR (50)   NOT NULL,
+    [FechaCreacion]     DATETIME       NOT NULL,
+    [FechaEnvio]        DATETIME       NULL,
+    [Error]             VARCHAR (MAX)  NULL,
+    CONSTRAINT [PK_tb_ColaCorreo] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdCorreo] ASC)
+);
+
---
+++ ERP_database/ERP_database/dbo/Tables/tb_ColaCorreoParametros.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[tb_ColaCorreoParametros] (
+    [IdEmpresa]             INT           NOT NULL,
+    [Usuario]               VARCHAR (500) NOT NULL,
+    [Contrasenia]           VARCHAR (500) NOT NULL,
+    [Puerto]                INT           NOT NULL,
+    [Host]                  VARCHAR (500) NOT NULL,
+    [PermitirSSL]           BIT           NOT NULL,
+    [CorreoCopia]           VARCHAR (200) NULL,
+    [IdUsuarioCreacion]     VARCHAR (200) NULL,
+    [FechaCreacion]         DATETIME      NULL,
+    [IdUsuarioModificacion] VARCHAR (200) NULL,
+    [FechaModificacion]     DATETIME      NULL,
+    CONSTRAINT [PK_tb_ColaCorreoParametros] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC)
+);
+

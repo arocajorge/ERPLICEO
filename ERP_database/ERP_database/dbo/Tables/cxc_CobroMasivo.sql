ccc2a97b0d5842034ed6b386131989df60cea97e
WendyCherres <wendycherres@gmail.com>
2020-07-07 16:56:59 -0500

    ..

---
+++ ERP_database/ERP_database/dbo/Tables/cxc_CobroMasivo.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[cxc_CobroMasivo] (
+    [IdEmpresa]             INT           NOT NULL,
+    [IdCobroMasivo]         NUMERIC (18)  NOT NULL,
+    [Fecha]                 DATETIME      NOT NULL,
+    [Observacion]           VARCHAR (MAX) NULL,
+    [Estado]                BIT           NOT NULL,
+    [IdUsuarioCreacion]     VARCHAR (200) NULL,
+    [FechaCreacion]         DATETIME      NULL,
+    [IdUsuarioModificacion] VARCHAR (200) NULL,
+    [FechaModificacion]     DATETIME      NULL,
+    [IdUsuarioAnulacion]    VARCHAR (200) NULL,
+    [FechaAnulacion]        DATETIME      NULL,
+    [MotivoAnulacion]       VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_cxc_CobroMasivo] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdCobroMasivo] ASC)
+);
+
---
+++ ERP_database/ERP_database/dbo/Tables/cxc_CobroMasivoDet.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[cxc_CobroMasivoDet] (
+    [IdEmpresa]     INT          NOT NULL,
+    [IdCobroMasivo] NUMERIC (18) NOT NULL,
+    [Secuencia]     INT          NOT NULL,
+    [IdAlumno]      NUMERIC (18) NOT NULL,
+    [Valor]         FLOAT (53)   NOT NULL,
+    [Fecha]         DATE         NOT NULL,
+    [IdSucursal]    INT          NOT NULL,
+    [IdCobro]       NUMERIC (18) NOT NULL,
+    CONSTRAINT [PK_cxc_CobroMasivoDet] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdCobroMasivo] ASC, [Secuencia] ASC),
+    CONSTRAINT [FK_cxc_CobroMasivoDet_cxc_cobro] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdCobro]) REFERENCES [dbo].[cxc_cobro] ([IdEmpresa], [IdSucursal], [IdCobro]),
+    CONSTRAINT [FK_cxc_CobroMasivoDet_cxc_CobroMasivo] FOREIGN KEY ([IdEmpresa], [IdCobroMasivo]) REFERENCES [dbo].[cxc_CobroMasivo] ([IdEmpresa], [IdCobroMasivo])
+);
+

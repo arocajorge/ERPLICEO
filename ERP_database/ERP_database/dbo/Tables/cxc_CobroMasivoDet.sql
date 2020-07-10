451de1687c28ceba2c3b1167a1eb3f3a9a7cb8fa
WendyCherres <wendycherres@gmail.com>
2020-07-09 23:45:40 -0500

    ...

--- ERP_database/ERP_database/dbo/Tables/cxc_CobroMasivoDet.sql
+++ ERP_database/ERP_database/dbo/Tables/cxc_CobroMasivoDet.sql
@@ -5,10 +5,12 @@
     [IdAlumno]      NUMERIC (18) NOT NULL,
     [Valor]         FLOAT (53)   NOT NULL,
     [Fecha]         DATE         NOT NULL,
-    [IdSucursal]    INT          NOT NULL,
-    [IdCobro]       NUMERIC (18) NOT NULL,
+    [IdSucursal]    INT          NULL,
+    [IdCobro]       NUMERIC (18) NULL,
     CONSTRAINT [PK_cxc_CobroMasivoDet] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdCobroMasivo] ASC, [Secuencia] ASC),
     CONSTRAINT [FK_cxc_CobroMasivoDet_cxc_cobro] FOREIGN KEY ([IdEmpresa], [IdSucursal], [IdCobro]) REFERENCES [dbo].[cxc_cobro] ([IdEmpresa], [IdSucursal], [IdCobro]),
     CONSTRAINT [FK_cxc_CobroMasivoDet_cxc_CobroMasivo] FOREIGN KEY ([IdEmpresa], [IdCobroMasivo]) REFERENCES [dbo].[cxc_CobroMasivo] ([IdEmpresa], [IdCobroMasivo])
 );
+
+
 
---
+++ ERP_database/ERP_database/dbo/Views/vwcxc_CobroMasivoDet.sql
@@ -0,0 +1,153 @@
+﻿CREATE VIEW dbo.vwcxc_CobroMasivoDet
+AS
+SELECT dbo.cxc_CobroMasivoDet.IdEmpresa, dbo.cxc_CobroMasivoDet.IdCobroMasivo, dbo.cxc_CobroMasivoDet.Secuencia, dbo.cxc_CobroMasivoDet.IdAlumno, dbo.aca_Alumno.Codigo, dbo.tb_persona.pe_nombreCompleto, 
+                  dbo.cxc_CobroMasivoDet.Valor, dbo.cxc_CobroMasivoDet.Fecha, dbo.cxc_CobroMasivoDet.IdSucursal, dbo.cxc_CobroMasivoDet.IdCobro
+FROM     dbo.aca_Alumno INNER JOIN
+                  dbo.cxc_CobroMasivoDet ON dbo.aca_Alumno.IdEmpresa = dbo.cxc_CobroMasivoDet.IdEmpresa AND dbo.aca_Alumno.IdAlumno = dbo.cxc_CobroMasivoDet.IdAlumno INNER JOIN
+                  dbo.tb_persona ON dbo.aca_Alumno.IdPersona = dbo.tb_persona.IdPersona
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPaneCount', @value = 1, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vwcxc_CobroMasivoDet';
+
+
+GO
+EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
+Begin DesignProperties = 
+   Begin PaneConfigurations = 
+      Begin PaneConfiguration = 0
+         NumPanes = 4
+         Configuration = "(H (1[40] 4[20] 2[20] 3) )"
+      End
+      Begin PaneConfiguration = 1
+         NumPanes = 3
+         Configuration = "(H (1 [50] 4 [25] 3))"
+      End
+      Begin PaneConfiguration = 2
+         NumPanes = 3
+         Configuration = "(H (1 [50] 2 [25] 3))"
+      End
+      Begin PaneConfiguration = 3
+         NumPanes = 3
+         Configuration = "(H (4 [30] 2 [40] 3))"
+      End
+      Begin PaneConfiguration = 4
+         NumPanes = 2
+         Configuration = "(H (1 [56] 3))"
+      End
+      Begin PaneConfiguration = 5
+         NumPanes = 2
+         Configuration = "(H (2 [66] 3))"
+      End
+      Begin PaneConfiguration = 6
+         NumPanes = 2
+         Configuration = "(H (4 [50] 3))"
+      End
+      Begin PaneConfiguration = 7
+         NumPanes = 1
+         Configuration = "(V (3))"
+      End
+      Begin PaneConfiguration = 8
+         NumPanes = 3
+         Configuration = "(H (1[56] 4[18] 2) )"
+      End
+      Begin PaneConfiguration = 9
+         NumPanes = 2
+         Configuration = "(H (1 [75] 4))"
+      End
+      Begin PaneConfiguration = 10
+         NumPanes = 2
+         Configuration = "(H (1[66] 2) )"
+      End
+      Begin PaneConfiguration = 11
+         NumPanes = 2
+         Configuration = "(H (4 [60] 2))"
+      End
+      Begin PaneConfiguration = 12
+         NumPanes = 1
+         Configuration = "(H (1) )"
+      End
+      Begin PaneConfiguration = 13
+         NumPanes = 1
+         Configuration = "(V (4))"
+      End
+      Begin PaneConfiguration = 14
+         NumPanes = 1
+         Configuration = "(V (2))"
+      End
+      ActivePaneConfig = 0
+   End
+   Begin DiagramPane = 
+      Begin Origin = 
+         Top = 0
+         Left = 0
+      End
+      Begin Tables = 
+         Begin Table = "aca_Alumno"
+            Begin Extent = 
+               Top = 0
+               Left = 310
+               Bottom = 163
+               Right = 555
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "cxc_CobroMasivoDet"
+            Begin Extent = 
+               Top = 0
+               Left = 15
+               Bottom = 291
+               Right = 259
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+         Begin Table = "tb_persona"
+            Begin Extent = 
+               Top = 7
+               Left = 633
+               Bottom = 170
+               Right = 907
+            End
+            DisplayFlags = 280
+            TopColumn = 0
+         End
+      End
+   End
+   Begin SQLPane = 
+   End
+   Begin DataPane = 
+      Begin ParameterDefaults = ""
+      End
+      Begin ColumnWidths = 10
+         Width = 284
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+         Width = 1200
+      End
+   End
+   Begin CriteriaPane = 
+      Begin ColumnWidths = 11
+         Column = 1440
+         Alias = 900
+         Table = 1170
+         Output = 720
+         Append = 1400
+         NewValue = 1170
+         SortType = 1350
+         SortOrder = 1410
+         GroupBy = 1350
+         Filter = 1350
+         Or = 1350
+         Or = 1350
+         Or = 1350
+      End
+   End
+End
+', @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'VIEW', @level1name = N'vwcxc_CobroMasivoDet';
+

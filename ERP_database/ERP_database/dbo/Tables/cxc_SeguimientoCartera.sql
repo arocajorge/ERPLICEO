b84d97f278dc68a5ff5953e758c1759635187cb1
WendyCherres <wendycherres@gmail.com>
2020-07-21 17:19:43 -0500

    ..

---
+++ ERP_database/ERP_database/dbo/Tables/cxc_SeguimientoCartera.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE [dbo].[cxc_SeguimientoCartera] (
+    [IdEmpresa]             INT           NOT NULL,
+    [IdSeguimiento]         INT           NOT NULL,
+    [IdAlumno]              NUMERIC (18)  NOT NULL,
+    [Fecha]                 DATETIME      NOT NULL,
+    [Observacion]           VARCHAR (MAX) NOT NULL,
+    [Estado]                BIT           NOT NULL,
+    [IdUsuarioCreacion]     VARCHAR (200) NULL,
+    [FechaCreacion]         DATETIME      NULL,
+    [IdUsuarioModificacion] VARCHAR (200) NULL,
+    [FechaModificacion]     DATETIME      NULL,
+    [IdUsuarioAnulacion]    VARCHAR (200) NULL,
+    [FechaAnulacion]        DATETIME      NULL,
+    [MotivoAnulacion]       VARCHAR (MAX) NULL,
+    CONSTRAINT [PK_cxc_SeguimientoCartera] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdSeguimiento] ASC),
+    CONSTRAINT [FK_cxc_SeguimientoCartera_aca_Alumno] FOREIGN KEY ([IdEmpresa], [IdAlumno]) REFERENCES [dbo].[aca_Alumno] ([IdEmpresa], [IdAlumno])
+);
+
--- ERP_database/ERP_database/dbo/Tables/tb_ColaCorreoCodigo.sql
+++ ERP_database/ERP_database/dbo/Tables/tb_ColaCorreoCodigo.sql
@@ -1,8 +1,11 @@
 ﻿CREATE TABLE [dbo].[tb_ColaCorreoCodigo] (
-    [IdEmpresa] INT            NOT NULL,
-    [Codigo]    VARCHAR (50)   NOT NULL,
-    [Asunto]    VARCHAR (1000) NOT NULL,
-    [Cuerpo]    VARCHAR (MAX)  NOT NULL,
+    [IdEmpresa]                  INT            NOT NULL,
+    [Codigo]                     VARCHAR (50)   NOT NULL,
+    [Asunto]                     VARCHAR (1000) NOT NULL,
+    [Cuerpo]                     VARCHAR (MAX)  NOT NULL,
+    [ApareceSeguimientoCobranza] BIT            NULL,
     CONSTRAINT [PK_tb_ColaCorreoCodigo] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [Codigo] ASC)
 );
+
+
 

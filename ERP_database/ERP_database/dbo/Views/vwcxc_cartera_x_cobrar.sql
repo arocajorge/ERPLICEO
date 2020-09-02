0f8eea1768b9e7fe90d81001c851cf8df6d1d0f9
arocajorge <arocajorge92@gmail.com>
2020-09-01 23:18:16 -0500

    RRR

--- ERP_database/ERP_database/dbo/Views/vwcxc_cartera_x_cobrar.sql
+++ ERP_database/ERP_database/dbo/Views/vwcxc_cartera_x_cobrar.sql
@@ -5,7 +5,9 @@ SELECT cabfac.IdEmpresa, cabfac.IdSucursal, cabfac.IdBodega, cabfac.vt_tipoDoc, 
                   ROUND(detfac.Total - ROUND(ISNULL(vwcxc_total_cobros_x_Docu.dc_ValorPago, 0), 2), 2) AS Saldo, ISNULL(vwcxc_total_cobros_x_Docu.dc_ValorPago, 0) AS TotalxCobrado, Bod.bo_Descripcion AS Bodega, 
                   CAST(detfac.SubtotalConDscto AS FLOAT) AS vt_Subtotal, CAST(detfac.ValorIVA AS FLOAT) AS vt_iva, cabfac.vt_fech_venc, ROUND(0, 2) AS dc_ValorRetFu, ROUND(0, 2) AS dc_ValorRetIva, Cli.Codigo AS CodCliente, 
                   tb_persona.pe_nombreCompleto AS NomCliente, tb_empresa.em_nombre, cabfac.Estado, CASE WHEN aca_AnioLectivo_Rubro.AplicaProntoPago = 1 AND aca_AnioLectivo_Periodo.FechaProntoPago >= CAST(GETDATE() AS DATE) 
-                  THEN detfac.ValorProntoPago ELSE detfac.Total END AS ValorProntoPago, aca_AnioLectivo_Periodo.FechaProntoPago AS FechaProntoPago, detfac.IdAnio, detfac.IdPlantilla, cabfac.IdPuntoVta
+                  THEN detfac.ValorProntoPago ELSE detfac.Total END AS ValorProntoPago, aca_AnioLectivo_Periodo.FechaProntoPago AS FechaProntoPago, detfac.IdAnio, detfac.IdPlantilla, cabfac.IdPuntoVta,
+				  CASE WHEN aca_AnioLectivo_Rubro.AplicaProntoPago = 1 AND aca_AnioLectivo_Periodo.FechaProntoPago >= CAST(GETDATE() AS DATE) 
+                  THEN detfac.ValorProntoPago - round(isnull(vwcxc_total_cobros_x_Docu.dc_ValorPago,0),2) ELSE ROUND(detfac.Total - ROUND(ISNULL(vwcxc_total_cobros_x_Docu.dc_ValorPago, 0), 2), 2) END AS SaldoProntoPago
 FROM     fa_factura_resumen AS detfac INNER JOIN
                   fa_factura AS cabfac ON detfac.IdBodega = cabfac.IdBodega AND detfac.IdSucursal = cabfac.IdSucursal AND detfac.IdEmpresa = cabfac.IdEmpresa AND detfac.IdCbteVta = cabfac.IdCbteVta INNER JOIN
                   tb_sucursal AS Sucu ON cabfac.IdEmpresa = Sucu.IdEmpresa AND cabfac.IdSucursal = Sucu.IdSucursal INNER JOIN
@@ -17,12 +19,12 @@ FROM     fa_factura_resumen AS detfac INNER JOIN
                   aca_AnioLectivo_Periodo ON detfac.IdPeriodo = aca_AnioLectivo_Periodo.IdPeriodo AND detfac.IdEmpresa = aca_AnioLectivo_Periodo.IdEmpresa LEFT OUTER JOIN
                   vwcxc_total_cobros_x_Docu ON cabfac.IdEmpresa = vwcxc_total_cobros_x_Docu.IdEmpresa AND cabfac.IdSucursal = vwcxc_total_cobros_x_Docu.IdSucursal AND cabfac.IdBodega = vwcxc_total_cobros_x_Docu.IdBodega_Cbte AND 
                   cabfac.vt_tipoDoc = vwcxc_total_cobros_x_Docu.dc_TipoDocumento AND cabfac.IdCbteVta = vwcxc_total_cobros_x_Docu.IdCbte_vta_nota
-WHERE  (cabfac.Estado = 'A') and ROUND(detfac.Total - ROUND(ISNULL(vwcxc_total_cobros_x_Docu.dc_ValorPago, 0), 2), 2) > 0
+WHERE  (cabfac.Estado = 'A') AND ROUND(detfac.Total - ROUND(ISNULL(vwcxc_total_cobros_x_Docu.dc_ValorPago, 0), 2), 2) > 0
 UNION
 SELECT A.IdEmpresa, A.IdSucursal, A.IdBodega, 'NTDB' AS CreDeb, CASE WHEN A.NumNota_Impresa IS NULL THEN 'N/D#' + CAST(A.IdNota AS varchar(20)) ELSE 'N/D#' + A.Serie1 + '-' + A.Serie2 + '' + A.NumNota_Impresa END AS Documento, 
                   A.sc_observacion, A.IdNota, A.CodNota, su.Su_Descripcion, A.IdCliente, A.IdAlumno, A.no_fecha, ROUND(SUM(B.sc_total), 2) AS sc_total, ROUND(SUM(B.sc_total) - ISNULL(SUM(CB.dc_ValorPago), 0), 2) AS Saldo, 
                   ISNULL(SUM(CB.dc_ValorPago), 0) AS totalCobrado, Bo.bo_Descripcion, ROUND(SUM(B.sc_subtotal), 2) AS sc_subtotal, ROUND(SUM(B.sc_iva), 2) AS sc_iva, A.no_fecha_venc, cast(0 AS float) AS RtFT, cast(0 AS float) AS RtIVA, 
-                  Cli.Codigo AS CodCliente, tb_persona.pe_nombreCompleto, tb_empresa.em_nombre, A.Estado, ROUND(SUM(B.sc_total), 2), NULL, NULL IdAnio, NULL IdPlantilla, A.IdPuntoVta
+                  Cli.Codigo AS CodCliente, tb_persona.pe_nombreCompleto, tb_empresa.em_nombre, A.Estado, ROUND(SUM(B.sc_total), 2), NULL, NULL IdAnio, NULL IdPlantilla, A.IdPuntoVta, ROUND(SUM(B.sc_total) - ISNULL(SUM(CB.dc_ValorPago), 0), 2) SaldoProntoPago
 FROM     fa_notaCreDeb AS A INNER JOIN
                   fa_notaCreDeb_det AS B ON A.IdEmpresa = B.IdEmpresa AND A.IdSucursal = B.IdSucursal AND A.IdBodega = B.IdBodega AND A.IdNota = B.IdNota INNER JOIN
                   tb_bodega AS Bo ON A.IdEmpresa = Bo.IdEmpresa AND A.IdSucursal = Bo.IdSucursal AND A.IdBodega = Bo.IdBodega INNER JOIN
@@ -38,7 +40,7 @@ WHERE  A.Estado = 'A' AND NOT EXISTS
                        WHERE   Cruce.IdEmpresa_nt = A.IdEmpresa AND Cruce.IdSucursal_nt = A.IdSucursal AND Cruce.IdBodega_nt = A.IdBodega AND Cruce.IdNota_nt = A.IdNota AND Cruce.Valor_Aplicado <> 0)
 GROUP BY A.IdEmpresa, A.IdSucursal, A.IdBodega, A.no_fecha, A.CreDeb, A.IdNota, A.Serie1, A.Serie2, A.NumNota_Impresa, A.sc_observacion, A.CodNota, su.Su_Descripcion, A.IdCliente, A.IdAlumno, Bo.bo_Descripcion, A.no_fecha_venc, 
                   Cli.Codigo, tb_persona.pe_nombreCompleto, tb_empresa.em_nombre, A.Estado, A.IdPuntoVta
-HAVING (A.CreDeb = 'D') and ROUND(SUM(B.sc_total) - ISNULL(SUM(CB.dc_ValorPago), 0), 2) > 0
+HAVING (A.CreDeb = 'D') AND ROUND(SUM(B.sc_total) - ISNULL(SUM(CB.dc_ValorPago), 0), 2) > 0
 GO
 EXECUTE sp_addextendedproperty @name = N'MS_DiagramPane1', @value = N'[0E232FF0-B466-11cf-A24F-00AA00A3EFFF, 1.00]
 Begin DesignProperties = 

1d46f081d1b74753ced44591d1d2c3aec9a5dfbf
WendyCherres <wendycherres@gmail.com>
2020-09-07 18:09:53 -0500

    ..

---
+++ ERP_database/ERP_database/Academico/Stored Procedures/SPACA_025.sql
@@ -0,0 +1,69 @@
+﻿
+CREATE PROCEDURE [Academico].[SPACA_025]
+(
+@IdEmpresa int,
+@IdAnio int,
+@IdSede int,
+@IdNivel int,
+@IdJornada int,
+@IdCurso int,
+@IdParalelo int,
+@IdParcial int,
+@IdMateria int,
+@MostrarRetirados bit
+)
+AS
+SELECT mp.IdEmpresa, mp.IdMatricula, mp.IdMateria, mp.IdCatalogoParcial, m.IdAnio, m.IdSede, m.IdNivel, m.IdJornada, m.IdCurso, m.IdParalelo, sn.NomSede, sn.NomNivel, sn.OrdenNivel, nj.NomJornada, nj.OrdenJornada, jc.NomCurso, 
+                  jc.OrdenCurso, cp.CodigoParalelo, cp.NomParalelo, cp.OrdenParalelo, m.IdAlumno, p.pe_nombreCompleto AS NombreAlumno, a.Codigo AS CodigoAlumno, p.pe_cedulaRuc, 
+				  case when ret.IdMatricula is not null then cast(1 as bit) else cast(0 as bit) end as EsRetirado,
+				  case when ret.IdMatricula is not null THEN 'RETIRADO' else 'ACTIVO' end as EsRetiradoString,
+				  cm.NomMateria, cm.NomMateriaArea, cm.NomMateriaGrupo, 
+                  cm.EsObligatorio, cm.OrdenMateria, cm.OrdenMateriaGrupo, cm.OrdenMateriaArea, pp.pe_nombreCompleto AS NombreProfesor, c.NomCatalogo, AN.Descripcion, mp.Calificacion1, mp.Calificacion2, mp.Calificacion3, mp.Calificacion4, 
+                  mp.Evaluacion, mp.MotivoCalificacion,mp.MotivoConducta, mp.AccionRemedial, equiv.Letra, equiv.Calificacion, 
+				  case when mp.IdCatalogoParcial = 28 then 
+				  prom.CalificacionP1 
+				  when mp.IdCatalogoParcial = 29 then 
+				  prom.CalificacionP2 
+				  when mp.IdCatalogoParcial = 30 then  
+				  prom.CalificacionP3 
+				  when mp.IdCatalogoParcial = 31 then  
+				  prom.CalificacionP4
+				  when mp.IdCatalogoParcial = 32 then 
+				  prom.CalificacionP5
+				  when mp.IdCatalogoParcial = 33 then 
+				  prom.CalificacionP6 END PromedioParcial,
+				  1 RowNumber
+FROM     aca_MatriculaCalificacion AS prom RIGHT OUTER JOIN
+                  aca_AnioLectivo_Curso_Materia AS cm INNER JOIN
+                  tb_persona AS p INNER JOIN
+                  aca_Alumno AS a ON p.IdPersona = a.IdPersona INNER JOIN
+                  aca_Matricula AS m ON a.IdEmpresa = m.IdEmpresa AND a.IdAlumno = m.IdAlumno INNER JOIN
+                  aca_MatriculaCalificacionParcial AS mp ON m.IdEmpresa = mp.IdEmpresa AND m.IdMatricula = mp.IdMatricula ON cm.IdEmpresa = m.IdEmpresa AND cm.IdAnio = m.IdAnio AND cm.IdSede = m.IdSede AND cm.IdNivel = m.IdNivel AND 
+                  cm.IdJornada = m.IdJornada AND cm.IdCurso = m.IdCurso AND cm.IdMateria = mp.IdMateria ON prom.IdEmpresa = mp.IdEmpresa AND prom.IdMatricula = mp.IdMatricula AND prom.IdMateria = mp.IdMateria LEFT OUTER JOIN
+                  aca_AnioLectivoConductaEquivalencia AS equiv ON m.IdEmpresa = equiv.IdEmpresa AND m.IdAnio = equiv.IdAnio AND mp.Conducta = equiv.Secuencia LEFT OUTER JOIN
+                  aca_AnioLectivo AS AN ON m.IdEmpresa = AN.IdEmpresa AND m.IdAnio = AN.IdAnio LEFT OUTER JOIN
+                  aca_Catalogo AS c ON mp.IdCatalogoParcial = c.IdCatalogo LEFT OUTER JOIN
+                  tb_persona AS pp INNER JOIN
+                  aca_Profesor AS pro ON pp.IdPersona = pro.IdPersona ON mp.IdEmpresa = pro.IdEmpresa AND mp.IdProfesor = pro.IdProfesor LEFT OUTER JOIN
+                  aca_AnioLectivo_Sede_NivelAcademico AS sn RIGHT OUTER JOIN
+                  aca_AnioLectivo_NivelAcademico_Jornada AS nj ON sn.IdEmpresa = nj.IdEmpresa AND sn.IdAnio = nj.IdAnio AND sn.IdSede = nj.IdSede AND sn.IdNivel = nj.IdNivel RIGHT OUTER JOIN
+                  aca_AnioLectivo_Jornada_Curso AS jc ON nj.IdEmpresa = jc.IdEmpresa AND nj.IdAnio = jc.IdAnio AND nj.IdSede = jc.IdSede AND nj.IdNivel = jc.IdNivel AND nj.IdJornada = jc.IdJornada RIGHT OUTER JOIN
+                  aca_AnioLectivo_Curso_Paralelo AS cp ON jc.IdEmpresa = cp.IdEmpresa AND jc.IdAnio = cp.IdAnio AND jc.IdSede = cp.IdSede AND jc.IdNivel = cp.IdNivel AND jc.IdJornada = cp.IdJornada AND jc.IdCurso = cp.IdCurso ON 
+                  m.IdEmpresa = cp.IdEmpresa AND m.IdAnio = cp.IdAnio AND m.IdSede = cp.IdSede AND m.IdNivel = cp.IdNivel AND m.IdJornada = cp.IdJornada AND m.IdCurso = cp.IdCurso AND m.IdParalelo = cp.IdParalelo
+				  left join
+					(
+					select r.IdEmpresa, r.IdMatricula 
+					from aca_AlumnoRetiro as r
+					where r.Estado = 1
+					) as ret on m.IdEmpresa =ret.IdEmpresa and m.IdMatricula = ret.IdMatricula
+where mp.IdEmpresa = @IdEmpresa 
+and m.IdAnio = @IdAnio
+and mp.IdCatalogoParcial = @IdParcial
+and mp.IdMateria = @IdMateria
+and m.IdSede = case when @IdSede = 0 then m.IdSede else @IdSede end
+and m.IdNivel = case when @IdNivel = 0 then m.IdNivel else @IdNivel end
+and m.IdJornada = case when @IdJornada = 0 then m.IdJornada else @IdJornada end
+and m.IdCurso = case when @IdCurso = 0 then m.IdCurso else @IdCurso end
+and m.IdParalelo = case when @IdParalelo = 0 then m.IdParalelo else @IdParalelo end
+and isnull(ret.IdMatricula,0) = case when @MostrarRetirados = 1 then isnull(ret.IdMatricula,0) else 0 end
+and a.Estado = 1
---
+++ ERP_database/ERP_database/Academico/Stored Procedures/SPACA_026.sql
@@ -0,0 +1,58 @@
+﻿
+CREATE PROCEDURE [Academico].[SPACA_026]
+(
+@IdEmpresa int,
+@IdAnio int,
+@IdSede int,
+@IdNivel int,
+@IdJornada int,
+@IdCurso int,
+@IdParalelo int,
+@IdMateria int,
+@IdCatalogoParcial int,
+@MostrarRetirados bit
+)
+AS
+SELECT mcc.IdEmpresa, mcc.IdMatricula, mcc.IdMateria, alu.Codigo, p.pe_nombreCompleto AS NombreAlumno, 
+case when ret.IdMatricula is not null then cast(1 as bit) else cast(0 as bit) end as EsRetirado,
+				  case when ret.IdMatricula is not null THEN 'RETIRADO' else 'ACTIVO' end as EsRetiradoString, 
+				  m.IdSede, m.IdAnio, m.IdNivel, m.IdJornada, m.IdCurso, m.IdParalelo, m.IdAlumno, AN.Descripcion, sn.NomSede, sn.NomNivel, sn.OrdenNivel, nj.NomJornada, 
+                  nj.OrdenJornada, jc.NomCurso, jc.OrdenCurso, cp.CodigoParalelo, cp.NomParalelo, cp.OrdenParalelo, MC.NomMateria, MC.NomMateriaArea, MC.NomMateriaGrupo, MC.EsObligatorio, MC.OrdenMateria, MC.OrdenMateriaGrupo, 
+                  MC.OrdenMateriaArea, mcc.IdCatalogoParcial, C.NomCatalogo, mcc.IdCalificacionCualitativa, acc.Codigo AS CodigoCalificacionCualitativa, acc.DescripcionCorta, mcc.IdProfesor, pp.pe_nombreCompleto AS NombreProfesor, 
+                  dbo.aca_AnioLectivoConductaEquivalencia.Letra, mcc.Conducta
+FROM     dbo.aca_AnioLectivoCalificacionCualitativa AS acc RIGHT OUTER JOIN
+                  dbo.aca_MatriculaCalificacionCualitativa AS mcc INNER JOIN
+                  dbo.aca_Matricula AS m ON mcc.IdEmpresa = m.IdEmpresa AND mcc.IdMatricula = m.IdMatricula INNER JOIN
+                  dbo.aca_Alumno AS alu ON m.IdEmpresa = alu.IdEmpresa AND m.IdAlumno = alu.IdAlumno INNER JOIN
+                  dbo.tb_persona AS p ON alu.IdPersona = p.IdPersona INNER JOIN
+                  dbo.aca_AnioLectivo_Curso_Materia AS MC ON m.IdEmpresa = MC.IdEmpresa AND m.IdAnio = MC.IdAnio AND m.IdSede = MC.IdSede AND m.IdNivel = MC.IdNivel AND m.IdJornada = MC.IdJornada AND m.IdCurso = MC.IdCurso AND 
+                  mcc.IdMateria = MC.IdMateria LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo AS AN ON m.IdEmpresa = AN.IdEmpresa AND m.IdAnio = AN.IdAnio LEFT OUTER JOIN
+                  dbo.tb_persona AS pp RIGHT OUTER JOIN
+                  dbo.aca_Profesor ON pp.IdPersona = dbo.aca_Profesor.IdPersona ON mcc.IdEmpresa = dbo.aca_Profesor.IdEmpresa AND mcc.IdProfesor = dbo.aca_Profesor.IdProfesor LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo_Curso_Paralelo AS cp ON m.IdEmpresa = cp.IdEmpresa AND m.IdAnio = cp.IdAnio AND m.IdSede = cp.IdSede AND m.IdNivel = cp.IdNivel AND m.IdJornada = cp.IdJornada AND m.IdCurso = cp.IdCurso AND 
+                  m.IdParalelo = cp.IdParalelo ON acc.IdEmpresa = mcc.IdEmpresa AND acc.IdCalificacionCualitativa = mcc.IdCalificacionCualitativa LEFT OUTER JOIN
+                  dbo.aca_Catalogo AS C ON mcc.IdCatalogoParcial = C.IdCatalogo LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo_Sede_NivelAcademico AS sn RIGHT OUTER JOIN
+                  dbo.aca_AnioLectivo_NivelAcademico_Jornada AS nj ON sn.IdEmpresa = nj.IdEmpresa AND sn.IdAnio = nj.IdAnio AND sn.IdSede = nj.IdSede AND sn.IdNivel = nj.IdNivel RIGHT OUTER JOIN
+                  dbo.aca_AnioLectivo_Jornada_Curso AS jc ON nj.IdEmpresa = jc.IdEmpresa AND nj.IdAnio = jc.IdAnio AND nj.IdSede = jc.IdSede AND nj.IdNivel = jc.IdNivel AND nj.IdJornada = jc.IdJornada ON cp.IdEmpresa = jc.IdEmpresa AND 
+                  cp.IdAnio = jc.IdAnio AND cp.IdSede = jc.IdSede AND cp.IdNivel = jc.IdNivel AND cp.IdJornada = jc.IdJornada AND cp.IdCurso = jc.IdCurso LEFT OUTER JOIN
+                  dbo.aca_AnioLectivoConductaEquivalencia ON mcc.IdEmpresa = dbo.aca_AnioLectivoConductaEquivalencia.IdEmpresa AND mcc.Conducta = dbo.aca_AnioLectivoConductaEquivalencia.Secuencia AND 
+                  m.IdAnio = dbo.aca_AnioLectivoConductaEquivalencia.IdAnio
+				  left join
+					(
+					select r.IdEmpresa, r.IdMatricula 
+					from aca_AlumnoRetiro as r
+					where r.Estado = 1
+					) as ret on m.IdEmpresa =ret.IdEmpresa and m.IdMatricula = ret.IdMatricula
+where m.IdEmpresa = @IdEmpresa 
+and m.IdAnio = @IdAnio
+and mcc.IdCatalogoParcial = @IdCatalogoParcial
+and mcc.IdMateria = @IdMateria
+and m.IdSede = case when @IdSede = 0 then m.IdSede else @IdSede end
+and m.IdNivel = case when @IdNivel = 0 then m.IdNivel else @IdNivel end
+and m.IdJornada = case when @IdJornada = 0 then m.IdJornada else @IdJornada end
+and m.IdCurso = case when @IdCurso = 0 then m.IdCurso else @IdCurso end
+and m.IdParalelo = case when @IdParalelo = 0 then m.IdParalelo else @IdParalelo end
+and isnull(ret.IdMatricula,0) = case when @MostrarRetirados = 1 then isnull(ret.IdMatricula,0) else 0 end
+and alu.Estado = 1

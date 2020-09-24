4635cacc3d909748c635ce468f4969df23b24bda
WendyCherres <wendycherres@gmail.com>
2020-09-24 16:40:17 -0500

    ...

--- ERP_database/ERP_database/Academico/Stored Procedures/SPACA_014.sql
+++ ERP_database/ERP_database/Academico/Stored Procedures/SPACA_014.sql
@@ -1,4 +1,7 @@
-﻿CREATE PROCEDURE [Academico].[SPACA_014]
+﻿
+
+
+CREATE PROCEDURE [Academico].[SPACA_014]
 (
 @IdEmpresa int,
 @IdAnio int,
@@ -7,31 +10,31 @@
 @IdJornada int,
 @IdCurso int,
 @IdParalelo int,
-@IdAlumno decimal,
+@IdAlumno numeric,
 @MostrarRetirados bit
 )
 AS
-SELECT mc.IdEmpresa, mc.IdMatricula, mc.IdMateria, alu.Codigo, p.pe_nombreCompleto, m.IdAnio, m.IdSede, m.IdNivel, m.IdJornada, m.IdCurso, m.IdParalelo, m.IdAlumno, AN.Descripcion, sn.NomSede, sn.NomNivel, sn.OrdenNivel, nj.NomJornada, 
+SELECT mc.IdEmpresa, m.IdAnio, m.IdSede, m.IdNivel, m.IdJornada, m.IdCurso, m.IdParalelo, m.IdAlumno, mc.IdMatricula, mc.IdMateria, 
+dbo.aca_AnioLectivo_Curso_Materia.NomMateria, dbo.aca_AnioLectivo_Curso_Materia.NomMateriaArea, dbo.aca_AnioLectivo_Curso_Materia.NomMateriaGrupo, 
+                  dbo.aca_AnioLectivo_Curso_Materia.EsObligatorio, dbo.aca_AnioLectivo_Curso_Materia.OrdenMateriaArea, dbo.aca_AnioLectivo_Curso_Materia.OrdenMateriaGrupo, dbo.aca_AnioLectivo_Curso_Materia.OrdenMateria, 
+alu.Codigo, p.pe_nombreCompleto, AN.Descripcion, sn.NomSede, sn.NomNivel, sn.OrdenNivel, nj.NomJornada, 
                   nj.OrdenJornada, jc.NomCurso, jc.OrdenCurso, cp.CodigoParalelo, cp.NomParalelo, cp.OrdenParalelo, 
-				  mc.CalificacionP1, mc.IdEquivalenciaPromedioP1, ep1.Codigo AS EquivalenciaPromedioP1,
-				  mc.CalificacionP2, mc.IdEquivalenciaPromedioP2, ep2.Codigo AS EquivalenciaPromedioP2,
-				  mc.CalificacionP3, mc.IdEquivalenciaPromedioP3, ep3.Codigo AS EquivalenciaPromedioP3,
+				  				  pins.pe_nombreCompleto as NombreInspector, pre.pe_nombreCompleto as NombreRepresentante,
+				  mc.CalificacionP1, ep1.Codigo AS EquivalenciaPromedioP1,
+				  mc.CalificacionP2, ep2.Codigo AS EquivalenciaPromedioP2,
+				  mc.CalificacionP3, ep3.Codigo AS EquivalenciaPromedioP3,
 				  mc.PromedioQ1, 
-				  mc.ExamenQ1, mc.IdEquivalenciaPromedioEQ1, epEQ1.Codigo AS EquivalenciaPromedioEQ1,
-				  mc.PromedioFinalQ1, mc.IdEquivalenciaPromedioQ1, epQ1.Codigo AS EquivalenciaPromedioQ1,
-				  mc.CalificacionP4, mc.IdEquivalenciaPromedioP4, ep4.Codigo AS EquivalenciaPromedioP4,
-                  mc.CalificacionP5, mc.IdEquivalenciaPromedioP5, ep5.Codigo AS EquivalenciaPromedioP5,
-				  mc.CalificacionP6, mc.IdEquivalenciaPromedioP6, ep6.Codigo AS EquivalenciaPromedioP6,
+				  mc.ExamenQ1, epEQ1.Codigo AS EquivalenciaPromedioEQ1,
+				  mc.PromedioFinalQ1, epQ1.Codigo AS EquivalenciaPromedioQ1,
+				  mc.CalificacionP4, ep4.Codigo AS EquivalenciaPromedioP4,
+                  mc.CalificacionP5, ep5.Codigo AS EquivalenciaPromedioP5,
+				  mc.CalificacionP6, ep6.Codigo AS EquivalenciaPromedioP6,
 				  mc.PromedioQ2, 
-				  mc.ExamenQ2, mc.IdEquivalenciaPromedioEQ2, epEQ2.Codigo AS EquivalenciaPromedioEQ2,
-				  mc.PromedioFinalQ2, mc.IdEquivalenciaPromedioQ2, epQ2.Codigo AS EquivalenciaPromedioQ2,
+				  mc.ExamenQ2, epEQ2.Codigo AS EquivalenciaPromedioEQ2,
+				  mc.PromedioFinalQ2, epQ2.Codigo AS EquivalenciaPromedioQ2,
 				  round(cast(((isnull(mc.PromedioFinalQ1,0)+isnull(mc.PromedioFinalQ2,0))/2) as float),2) as PromedioQuimestralFinal, 
 				  mc.ExamenMejoramiento, mc.CampoMejoramiento, mc.ExamenSupletorio, mc.ExamenRemedial, mc.ExamenGracia, 
-				  mc.PromedioFinal, mc.IdEquivalenciaPromedioPF, epPF.Codigo AS EquivalenciaPromedioPF,
-                  dbo.aca_AnioLectivo_Curso_Materia.NomMateria, dbo.aca_AnioLectivo_Curso_Materia.NomMateriaArea, dbo.aca_AnioLectivo_Curso_Materia.NomMateriaGrupo, 
-                  dbo.aca_AnioLectivo_Curso_Materia.EsObligatorio, dbo.aca_AnioLectivo_Curso_Materia.OrdenMateria, 
-				  dbo.aca_AnioLectivo_Curso_Materia.OrdenMateriaGrupo, dbo.aca_AnioLectivo_Curso_Materia.OrdenMateriaArea,
-				  cp.IdProfesorInspector,pins.pe_nombreCompleto as NombreInspector, pre.pe_nombreCompleto as NombreRepresentante
+				  mc.PromedioFinal, mc.IdEquivalenciaPromedioPF, epPF.Codigo AS EquivalenciaPromedioPF
 FROM     dbo.aca_MatriculaCalificacion AS mc INNER JOIN
                   dbo.aca_Matricula AS m ON mc.IdEmpresa = m.IdEmpresa AND mc.IdMatricula = m.IdMatricula INNER JOIN
                   dbo.aca_Alumno AS alu ON m.IdEmpresa = alu.IdEmpresa AND m.IdAlumno = alu.IdAlumno INNER JOIN
@@ -68,15 +71,81 @@ FROM     dbo.aca_MatriculaCalificacion AS mc INNER JOIN
 					) as ret on m.IdEmpresa =ret.IdEmpresa and m.IdMatricula = ret.IdMatricula
 where mc.IdEmpresa = @IdEmpresa 
 and m.IdAnio = @IdAnio
---and m.IdSede = @IdSede
---and m.IdNivel = @IdNivel
---and m.IdJornada = @IdJornada
---and m.IdCurso = @IdCurso
---and m.IdParalelo = @IdParalelo
-and m.IdSede = case when @IdSede = 0 then m.IdSede else @IdSede end
+and m.IdSede = @IdSede
+and m.IdJornada = @IdJornada
 and m.IdNivel = case when @IdNivel = 0 then m.IdNivel else @IdNivel end
-and m.IdJornada = case when @IdJornada = 0 then m.IdJornada else @IdJornada end
 and m.IdCurso = case when @IdCurso = 0 then m.IdCurso else @IdCurso end
 and m.IdParalelo = case when @IdParalelo = 0 then m.IdParalelo else @IdParalelo end
 and m.IdAlumno = case when @IdAlumno = 0 then m.IdAlumno else @IdAlumno end
 and isnull(ret.IdMatricula,0) = case when @MostrarRetirados = 1 then isnull(ret.IdMatricula,0) else 0 end
+UNION ALL
+(
+
+--- CALIFICACIONES CUALITATIVAS  
+select a.IdEmpresa, a.IdAnio, a.IdSede, a.IdNivel, a.IdJornada, a.IdCurso, a.IdParalelo, a.IdAlumno, a.IdMatricula, a.IdMateria, 
+a.NomMateria, a.NomMateriaArea, a.NomMateriaGrupo, a.EsObligatorio, a.OrdenMateriaArea, a.OrdenMateriaGrupo, a.OrdenMateria,
+a.Codigo, a.NombreAlumno, a.Descripcion,a.NomSede, a.NomNivel, a.OrdenNivel, a.NomJornada, 
+a.OrdenJornada, a.NomCurso, a.OrdenCurso, a.CodigoParalelo, a.NomParalelo, a.OrdenParalelo, 
+a.NombreInspector, a.NombreRepresentante,
+max(a.CalificacionP1) CalificacionP1, null EquivalenciaP1,
+max(a.CalificacionP2) CalificacionP2, null EquivalenciaP2,
+max(a.CalificacionP3) CalificacionP3, null EquivalenciaP3,
+null PromedioQ1, null ExamenQ1, null EquivalenciaPromedioEQ1, null PromedioFinalQ1, null EquivalenciaPromedioQ1,
+max(a.CalificacionP4) CalificacionP4,null EquivalenciaP4, 
+max(a.CalificacionP5) CalificacionP5, null EquivalenciaP5, 
+max(a.CalificacionP6) CalificacionP6, null EquivalenciaP6,
+null PromedioQ2, null ExamenQ2, null EquivalenciaPromedioEQ2, null PromedioFinalQ2, null EquivalenciaPromedioQ2,
+null PromedioQuimestralFinal, null ExamenMejoramiento, null CampoMejoramiento, null ExamenSupletorio, null ExamenRemedial, null ExamenGracia, 
+null PromedioFinal, null IdEquivalenciaPromedioPF, null EquivalenciaPromedioPF
+from(
+SELECT a.IdEmpresa, a.IdMatricula, a.IdMateria, m.IdAnio, m.IdSede, m.IdNivel, m.IdJornada, m.IdCurso, m.IdParalelo, m.IdAlumno, MC.NomMateria, MC.NomMateriaArea, MC.NomMateriaGrupo, MC.EsObligatorio, MC.OrdenMateria, 
+                  MC.OrdenMateriaGrupo, MC.OrdenMateriaArea, alu.Codigo, p.pe_nombreCompleto NombreAlumno, AN.Descripcion, sn.NomSede, sn.NomNivel, sn.OrdenNivel, nj.NomJornada, nj.OrdenJornada, jc.NomCurso, jc.OrdenCurso, cp.CodigoParalelo, 
+                  cp.NomParalelo, cp.OrdenParalelo, pins.pe_nombreCompleto AS NombreInspector, pre.pe_nombreCompleto NombreRepresentante,
+				  CASE WHEN IdCatalogoParcial = 28 THEN acc.Codigo END AS CalificacionP1, 
+				  CASE WHEN IdCatalogoParcial = 29 THEN acc.Codigo END AS CalificacionP2, 
+                  CASE WHEN IdCatalogoParcial = 30 THEN acc.Codigo END AS CalificacionP3, 
+				  CASE WHEN IdCatalogoParcial = 31 THEN acc.Codigo END AS CalificacionP4, 
+				  CASE WHEN IdCatalogoParcial = 32 THEN acc.Codigo END AS CalificacionP5, 
+                  CASE WHEN IdCatalogoParcial = 33 THEN acc.Codigo END AS CalificacionP6
+FROM     dbo.aca_MatriculaCalificacionCualitativa AS a INNER JOIN
+                  dbo.aca_Matricula AS m ON m.IdEmpresa = a.IdEmpresa AND m.IdMatricula = a.IdMatricula INNER JOIN
+                  dbo.aca_Alumno AS alu ON m.IdEmpresa = alu.IdEmpresa AND m.IdAlumno = alu.IdAlumno INNER JOIN
+                  dbo.tb_persona AS p ON alu.IdPersona = p.IdPersona LEFT OUTER JOIN
+                  dbo.aca_AnioLectivoCalificacionCualitativa AS acc ON acc.IdEmpresa = a.IdEmpresa AND acc.IdAnio = m.IdAnio AND acc.IdCalificacionCualitativa = a.IdCalificacionCualitativa LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo AS AN ON m.IdEmpresa = AN.IdEmpresa AND m.IdAnio = AN.IdAnio LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo_Curso_Paralelo AS cp RIGHT OUTER JOIN
+                  dbo.aca_AnioLectivo_Jornada_Curso AS jc LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo_Sede_NivelAcademico AS sn INNER JOIN
+                  dbo.aca_AnioLectivo_NivelAcademico_Jornada AS nj ON sn.IdEmpresa = nj.IdEmpresa AND sn.IdAnio = nj.IdAnio AND sn.IdSede = nj.IdSede AND sn.IdNivel = nj.IdNivel ON jc.IdJornada = nj.IdJornada AND jc.IdEmpresa = nj.IdEmpresa AND 
+                  jc.IdAnio = nj.IdAnio AND jc.IdSede = nj.IdSede AND jc.IdNivel = nj.IdNivel LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo_Curso_Materia AS MC ON jc.IdEmpresa = MC.IdEmpresa AND jc.IdAnio = MC.IdAnio AND jc.IdSede = MC.IdSede AND jc.IdNivel = MC.IdNivel AND jc.IdJornada = MC.IdJornada AND jc.IdCurso = MC.IdCurso ON 
+                  cp.IdEmpresa = jc.IdEmpresa AND cp.IdAnio = jc.IdAnio AND cp.IdSede = jc.IdSede AND cp.IdNivel = jc.IdNivel AND cp.IdJornada = jc.IdJornada ON m.IdEmpresa = MC.IdEmpresa AND m.IdAnio = MC.IdAnio AND m.IdSede = MC.IdSede AND 
+                  m.IdNivel = MC.IdNivel AND m.IdJornada = MC.IdJornada AND m.IdCurso = MC.IdCurso AND a.IdMateria = MC.IdMateria AND m.IdEmpresa = sn.IdEmpresa AND m.IdAnio = sn.IdAnio AND m.IdSede = sn.IdSede AND m.IdNivel = sn.IdNivel AND
+                   m.IdEmpresa = cp.IdEmpresa AND m.IdAnio = cp.IdAnio AND m.IdSede = cp.IdSede AND m.IdNivel = cp.IdNivel AND m.IdJornada = cp.IdJornada AND m.IdCurso = cp.IdCurso AND m.IdParalelo = cp.IdParalelo
+LEFT OUTER JOIN aca_Profesor AS pro ON cp.IdEmpresa = pro.IdEmpresa and cp.IdProfesorInspector = pro.IdProfesor
+				  LEFT OUTER JOIN tb_persona as pins on pins.IdPersona = pro.IdPersona
+				  LEFT OUTER JOIN tb_persona as pre on pre.IdPersona = m.IdPersonaR
+LEFT JOIN	
+					(
+					select r.IdEmpresa, r.IdMatricula 
+					from aca_AlumnoRetiro as r
+					where r.Estado = 1
+					) as ret on m.IdEmpresa =ret.IdEmpresa and m.IdMatricula = ret.IdMatricula
+--WHERE  (a.IdEmpresa = 1) AND (m.IdAnio = 1) AND (m.IdSede = 1) AND (m.IdNivel = 4) AND (m.IdJornada = 1) AND (m.IdCurso = 7) AND (m.IdParalelo = 1)
+where mc.IdEmpresa = @IdEmpresa 
+and m.IdAnio = @IdAnio
+and m.IdSede = @IdSede
+and m.IdJornada = @IdJornada
+and m.IdNivel = case when @IdNivel = 0 then m.IdNivel else @IdNivel end
+and m.IdCurso = case when @IdCurso = 0 then m.IdCurso else @IdCurso end
+and m.IdParalelo = case when @IdParalelo = 0 then m.IdParalelo else @IdParalelo end
+and m.IdAlumno = case when @IdAlumno = 0 then m.IdAlumno else @IdAlumno end
+and isnull(ret.IdMatricula,0) = case when @MostrarRetirados = 1 then isnull(ret.IdMatricula,0) else 0 end
+) a
+group by
+a.IdEmpresa, a.IdAnio, a.IdSede, a.IdNivel, a.IdJornada, a.IdCurso, a.IdParalelo, a.IdAlumno, a.IdMatricula, a.IdMateria, 
+a.NomMateria, a.NomMateriaArea, a.NomMateriaGrupo, a.EsObligatorio, a.OrdenMateriaArea, a.OrdenMateriaGrupo, a.OrdenMateria,
+a.Codigo, a.NombreAlumno, a.Descripcion,a.NomSede, a.NomNivel, a.OrdenNivel, a.NomJornada, 
+a.OrdenJornada, a.NomCurso, a.OrdenCurso, a.CodigoParalelo, a.NomParalelo, a.OrdenParalelo, a.NombreInspector, a.NombreRepresentante
+-----------------
+)

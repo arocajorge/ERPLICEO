f47038c567bd9de987c67e41ec2e94ce4e5f8858
WendyCherres <wendycherres@gmail.com>
2020-09-28 07:52:57 -0500

    ..

---
+++ ERP_database/ERP_database/Academico/Stored Procedures/SPACA_034.sql
@@ -0,0 +1,57 @@
+﻿create PROCEDURE [Academico].[SPACA_034]
+(
+@IdEmpresa int,
+@IdAnio int,
+@IdSede int,
+@IdNivel int,
+@IdJornada int,
+@IdCurso int,
+@IdParalelo int,
+@IdAlumno numeric,
+@MostrarRetirados bit
+)
+AS
+SELECT mc.IdEmpresa, m.IdAnio, m.IdSede, m.IdNivel, m.IdJornada, m.IdCurso, m.IdParalelo, m.IdAlumno,  mc.IdMatricula,mc.IdMateria, 
+                  CM.NomMateria, CM.NomMateriaArea, 
+                  CM.NomMateriaGrupo, CM.EsObligatorio, 
+				  ISNULL(CM.OrdenMateria,0) OrdenMateria, ISNULL(CM.OrdenMateriaGrupo,2) OrdenMateriaGrupo, 
+                  ISNULL(CM.OrdenMateriaArea,0) OrdenMateriaArea, 
+alu.Codigo, p.pe_nombreCompleto, AN.Descripcion, sn.NomSede, sn.NomNivel, sn.OrdenNivel, nj.NomJornada, 
+                  nj.OrdenJornada, jc.NomCurso, jc.OrdenCurso, cp.CodigoParalelo, cp.NomParalelo, cp.OrdenParalelo, 
+				  cast(mc.PromedioFinalQ1 as varchar) PromedioFinalQ1, 
+				  cast (mc.PromedioFinalQ2 as varchar) PromedioFinalQ2,
+				  case when mc.ExamenSupletorio < 7 then 
+					case when mc.ExamenRemedial < 7 then cast(mc.ExamenGracia as varchar) else cast(mc.ExamenRemedial as varchar) end  
+				  else cast(mc.ExamenSupletorio as varchar)
+				  end Supletorio,
+				  cast(mc.ExamenSupletorio as varchar) ExamenSupletorio, 
+				  cast(mc.ExamenRemedial as varchar) ExamenRemedial, 
+				  cast(mc.ExamenGracia as varchar) ExamenGracia, 
+				  cast(mc.PromedioFinal as varchar) PromedioFinal
+FROM     dbo.aca_MatriculaCalificacion AS mc INNER JOIN
+                  dbo.aca_Matricula AS m ON mc.IdEmpresa = m.IdEmpresa AND mc.IdMatricula = m.IdMatricula INNER JOIN
+                  dbo.aca_Alumno AS alu ON m.IdEmpresa = alu.IdEmpresa AND m.IdAlumno = alu.IdAlumno INNER JOIN
+                  dbo.tb_persona AS p ON alu.IdPersona = p.IdPersona INNER JOIN
+                  dbo.aca_AnioLectivo_Curso_Materia CM ON m.IdEmpresa = CM.IdEmpresa AND m.IdAnio = CM.IdAnio AND m.IdSede = CM.IdSede AND 
+                  m.IdNivel = CM.IdNivel AND m.IdJornada = CM.IdJornada AND m.IdCurso = CM.IdCurso AND 
+                  mc.IdMateria = CM.IdMateria 
+				  LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo AS AN ON m.IdEmpresa = AN.IdEmpresa AND m.IdAnio = AN.IdAnio LEFT OUTER JOIN
+                  dbo.aca_AnioLectivo_Sede_NivelAcademico AS sn RIGHT OUTER JOIN
+                  dbo.aca_AnioLectivo_NivelAcademico_Jornada AS nj ON sn.IdEmpresa = nj.IdEmpresa AND sn.IdAnio = nj.IdAnio AND sn.IdSede = nj.IdSede AND sn.IdNivel = nj.IdNivel RIGHT OUTER JOIN
+                  dbo.aca_AnioLectivo_Jornada_Curso AS jc ON nj.IdEmpresa = jc.IdEmpresa AND nj.IdAnio = jc.IdAnio AND nj.IdSede = jc.IdSede AND nj.IdNivel = jc.IdNivel AND nj.IdJornada = jc.IdJornada RIGHT OUTER JOIN
+                  dbo.aca_AnioLectivo_Curso_Paralelo AS cp ON jc.IdEmpresa = cp.IdEmpresa AND jc.IdAnio = cp.IdAnio AND jc.IdSede = cp.IdSede AND jc.IdNivel = cp.IdNivel AND jc.IdJornada = cp.IdJornada AND jc.IdCurso = cp.IdCurso ON 
+                  m.IdEmpresa = cp.IdEmpresa AND m.IdAnio = cp.IdAnio AND m.IdSede = cp.IdSede AND m.IdNivel = cp.IdNivel AND m.IdJornada = cp.IdJornada AND m.IdCurso = cp.IdCurso AND m.IdParalelo = cp.IdParalelo 
+				  LEFT OUTER JOIN
+                      (SELECT IdEmpresa, IdMatricula
+                       FROM      dbo.aca_AlumnoRetiro AS r
+                       WHERE   (Estado = 1)) AS ret ON m.IdEmpresa = ret.IdEmpresa AND m.IdMatricula = ret.IdMatricula
+where mc.IdEmpresa = @IdEmpresa 
+and m.IdAnio = @IdAnio
+and m.IdSede = @IdSede
+and m.IdNivel = @IdNivel
+and m.IdJornada = @IdJornada
+and m.IdCurso = @IdCurso
+and m.IdParalelo = case when @IdParalelo = 0 then m.IdParalelo else @IdParalelo end
+and m.IdAlumno = case when @IdAlumno = 0 then m.IdAlumno else @IdAlumno end
+and isnull(ret.IdMatricula,0) = case when @MostrarRetirados = 1 then isnull(ret.IdMatricula,0) else 0 end

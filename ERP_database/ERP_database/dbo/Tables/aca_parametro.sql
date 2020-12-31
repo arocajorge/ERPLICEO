b0568592babb37373060824fe87f41894c596532
WendyCherres <wendycherres@gmail.com>
2020-12-31 12:27:46 -0500

    ...

---
+++ ERP_database/ERP_database/dbo/Tables/aca_parametro.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[aca_parametro] (
+    [IdEmpresa]              INT           NOT NULL,
+    [RutaImagen_Alumno]      VARCHAR (500) NULL,
+    [RutaImagen_Profesor]    VARCHAR (500) NULL,
+    [RutaImagen_Seguimiento] VARCHAR (500) NULL,
+    CONSTRAINT [PK_aca_parametro] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC)
+);
+

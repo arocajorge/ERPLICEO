3233d0fbbcd8b1e2f47a534c8f404e9094c9c26e
WendyCherres <wendycherres@gmail.com>
2021-01-02 20:38:29 -0500

    ...

--- ERP_database/ERP_database/dbo/Tables/aca_MecanismoDePago.sql
+++ ERP_database/ERP_database/dbo/Tables/aca_MecanismoDePago.sql
@@ -3,6 +3,7 @@
     [IdMecanismo]               NUMERIC (18)  NOT NULL,
     [NombreMecanismo]           VARCHAR (500) NULL,
     [IdTerminoPago]             VARCHAR (20)  NOT NULL,
+    [IdEmpresa_rol]             INT           NULL,
     [Estado]                    BIT           NULL,
     [IdUsuarioCreacion]         VARCHAR (200) NULL,
     [FechaCreacion]             DATETIME      NULL,
@@ -13,8 +14,11 @@
     [MotivoAnulacion]           VARCHAR (MAX) NULL,
     [IdTipoNotaDescuentoPorRol] INT           NULL,
     CONSTRAINT [PK_aca_MecanismoDePago] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC, [IdMecanismo] ASC),
-    CONSTRAINT [FK_aca_MecanismoDePago_fa_TerminoPago] FOREIGN KEY ([IdTerminoPago]) REFERENCES [dbo].[fa_TerminoPago] ([IdTerminoPago])
+    CONSTRAINT [FK_aca_MecanismoDePago_fa_TerminoPago] FOREIGN KEY ([IdTerminoPago]) REFERENCES [dbo].[fa_TerminoPago] ([IdTerminoPago]),
+    CONSTRAINT [FK_aca_MecanismoDePago_tb_empresa] FOREIGN KEY ([IdEmpresa_rol]) REFERENCES [dbo].[tb_empresa] ([IdEmpresa])
 );
+
+
 
 
 

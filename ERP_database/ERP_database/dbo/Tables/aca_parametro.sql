1d0e3a65ac5e9a3ce1b3f757001baed938de5643
WendyCherres <wendycherres@gmail.com>
2021-02-12 16:20:52 -0500

    ..

--- ERP_database/ERP_database/dbo/Tables/aca_parametro.sql
+++ ERP_database/ERP_database/dbo/Tables/aca_parametro.sql
@@ -3,6 +3,11 @@
     [RutaImagen_Alumno]      VARCHAR (500) NULL,
     [RutaImagen_Profesor]    VARCHAR (500) NULL,
     [RutaImagen_Seguimiento] VARCHAR (500) NULL,
+    [FtpUser]                VARCHAR (500) NULL,
+    [FtpPassword]            VARCHAR (500) NULL,
+    [FtpUrl]                 VARCHAR (500) NULL,
     CONSTRAINT [PK_aca_parametro] PRIMARY KEY CLUSTERED ([IdEmpresa] ASC)
 );
+
+
 
